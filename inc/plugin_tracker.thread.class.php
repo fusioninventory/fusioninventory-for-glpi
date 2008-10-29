12012833486d3617eb623346aadde1e67994888b
David Durieux <d.durieux@siprossii.com>
2008-10-29 23:20:13 +0000

    [tracker] Plugin in progress

--- inc/plugin_tracker.thread.class.php
+++ inc/plugin_tracker.thread.class.php
@@ -39,6 +39,14 @@ class Threads extends CommonDBTM
 
 		global $DB,$LANG,$LANGTRACKER;
 
+		$minfreq = 9999;
+
+		$sql = 	"SELECT ID, process_id, SUM(network_queries) AS network_queries, status, COUNT(*) AS threads_number, " .
+			"MIN(start_time) AS starting_date, MAX(end_time) AS ending_date, TIME_TO_SEC(MAX(end_time))-TIME_TO_SEC(MIN(start_time)) AS duree, " .
+			"end_time >= DATE_ADD(NOW(), INTERVAL -" . $minfreq . " HOUR) AS DoStat ".
+		      	"FROM glpi_plugin_tracker_processes GROUP BY process_id ORDER BY ID DESC";
+	     	$result = $DB->query($sql);
+
 		echo "<div id='barre_onglets'><ul id='onglet'>\n";
 		echo "<li class='actif'><a href='" . $_SERVER["PHP_SELF"] . "'>&nbsp;".$LANGTRACKER["processes"][0]."&nbsp;</a></li>\n";
 		echo "<li><a href=''>&nbsp;".$LANGTRACKER["processes"][11]."&nbsp;</a></li>\n";
@@ -63,6 +71,75 @@ class Threads extends CommonDBTM
 		echo"<th>".$LANGTRACKER["processes"][9]."</th>";
 		echo"<th>".$LANGTRACKER["processes"][10]."</th>";		
 		echo "</th></tr>\n";
+		
+// VERIFIER
+
+		if ($DB->numrows($result)) {
+			while ($thread = $DB->fetch_array($result)){
+				//if ($config->fields["display_empty"] || $thread["status"] != STATE_FINISHED || (!$config->fields["display_empty"] && $thread["total_machines"] > 0 && $thread["status"] == STATE_FINISHED))
+				//{
+					echo "<tr class='tab_bg_1'>"; 
+					
+					//if ($canedit){
+					//	echo "<td width='10'>";
+					//	$sel="";
+					//	if (isset($_GET["select"])&&$_GET["select"]=="all") $sel="checked";
+					//	echo "<input type='checkbox' name='item[".$thread["process_id"]."]' value='1' $sel>";
+					//	echo "</td>";
+					//}
+					//else	 echo "<td width='10'>&nbsp;</td>";
+					echo "<td width='10'>&nbsp;</td>";
+				
+					echo "<td align='center'><a href=\"./plugin_mass_ocs_import.process.form.php?pid=".$thread["process_id"]."\">".$thread["process_id"]."</a></td>";
+					echo "<td align='center'>";
+					
+					switch(getProcessStatus($thread["process_id"]))
+					{
+						case STATE_FINISHED :
+							echo "<img src='../pics/export.png'>";
+							break;
+						case STATE_RUNNING :
+							echo "<img src='../pics/wait.png'>";
+							break;
+						case STATE_STARTED :
+							echo "<img src='../pics/ok2.png'>";
+							break;
+					}
+						
+					echo "</td>";
+					echo "<td align='center'>".$thread["threads_number"]."</td>";
+					echo "<td align='center'>".convDateTime($thread["starting_date"])."</td>";
+					echo "<td align='center'>".convDateTime($thread["ending_date"])."</td>";
+					echo "<td align='center'></td>";
+					echo "<td align='center'></td>";
+					echo "<td align='center'></td>";
+					echo "<td align='center'></td>";
+					
+					echo "<td align='center'>";
+					if ($thread["status"] == STATE_FINISHED)
+						echo timestampToString($thread["duree"]);
+					else
+						echo "-----";	
+					
+					echo "</td>";
+					
+					//echo "<td align='center'>"; 
+					//if ($thread["ocs_server_id"] != -1)
+					//{
+					//	$ocsConfig = getOcsConf($thread["ocs_server_id"]);
+					//	echo "<a href=\"".GLPI_ROOT."/front/ocsng.form.php?ID=".$ocsConfig["ID"]."\">".$ocsConfig["name"]."</a>";
+					//}
+					//else
+					//	echo $OCSMASSIMPORTLANG["config"][22];
+						
+					//echo "</td>";
+					echo "</tr>\n";
+				//}	
+			}
+		}
+
+// FIN DES CHOSES A VERIFIER		
+		
 		
 		echo "</table>";
 
