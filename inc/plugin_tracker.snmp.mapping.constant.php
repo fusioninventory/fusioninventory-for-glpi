53cc9e81b2c587ed781522c7db99f76dfb21c47b
David Durieux <d.durieux@siprossii.com>
2008-11-11 17:13:10 +0000

    [tracker] Add Global $TRACKER_MAPPING

--- inc/plugin_tracker.snmp.mapping.constant.php
+++ inc/plugin_tracker.snmp.mapping.constant.php
@@ -37,7 +37,7 @@ if (!defined('GLPI_ROOT')){
 	die("Sorry. You can't access directly to this file");
 }
 
-global $LANG,$LANGTRACKER;
+global $LANG,$LANGTRACKER,$TRACKER_MAPPING;
 
 // ----------------------------------------------------------------------
 //NETWORK MAPPING MAPPING
