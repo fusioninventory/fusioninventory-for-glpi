82dda17867ce20a5cc943dc67553c55364a96c29
David Durieux <d.durieux@siprossii.com>
2008-11-12 00:08:47 +0000

    [tracker] Begin to rewrite file with right classes and move functions to function file

--- inc/plugin_tracker.snmp.history.classes.php
+++ inc/plugin_tracker.snmp.history.classes.php
@@ -29,7 +29,7 @@
  */
 
 // ----------------------------------------------------------------------
-// Original Author of file: Nicolas SMOLYNIEC
+// Original Author of file: DURIEUX David
 // Purpose of file:
 // ----------------------------------------------------------------------
 
@@ -38,162 +38,90 @@ if (!defined('GLPI_ROOT')){
 }
 // Modification by David
 // Remplacement de plugin_tracker_snmp en plugin_tracker_snmp2
-class plugin_tracker_SNMP_log extends CommonDBTM {
+class plugin_tracker_SNMP_history extends CommonDBTM {
 
-
-	function addLog()
-	{
-		global $DB;
-
-			
-	
-	
-	}
-	
-	
-	// $status = connection or disconnection	
-	function addLogConnection($status,$port_networking,$ID2=0)
+	/**
+	 * Insert port history with connection and disconnection
+	 *
+	 * @param $status status of port ('make' or 'remove')
+	 * @param $array with values : $array["FK_ports"], $array["value"], $array["device_type"] and $array["device_ID"]
+	 *
+	 * @return ID of inserted line
+	 *
+	**/
+	function insert_connection($status,$array)
 	{
 		global $DB,$CFG_GLPI;
-		$CommonItem = new CommonItem;
-		// Récupérer le port de la machine associé au port du switch
-		$nw=new Netwire;
-		if ($ID2 == "0")
-			$ID2=$nw->getOppositeContact($port_networking);
-		// Récupérer le type de matériel
-		$netport=new Netport;
-		$netport->getFromDB($ID2);
-		$device_type = $netport->fields["device_type"];
-		// Récupérer l'adresse MAC
-		$netport->getFromDB($ID2);
-		$ifmac = $netport->fields["ifmac"];
-		// Récupération de l'id du matériel
-		$mat_ID = $netport->fields["on_device"];
-echo ">>>> LOG-JOURNAL ".$mat_ID."<<<<\n";
-		// Ajouter en DB
 		if ($status == "remove")
 		{
 			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
 			(FK_ports,old_value,old_device_type,old_device_ID,date_mod)
-			VALUES('".$port_networking."','".$ifmac."','".$device_type."','".$mat_ID."','".date("Y-m-d H:i:s")."')";
+			VALUES('".$array["FK_ports"]."','".$array["value"]."','".$array["device_type"]."','".$array["device_ID"]."','".date("Y-m-d H:i:s")."')";
+		
 		}
 		else if ($status == "make")
 		{
 			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
 			(FK_ports,new_value,new_device_type,new_device_ID,date_mod)
-			VALUES('".$port_networking."','".$ifmac."','".$device_type."','".$mat_ID."','".date("Y-m-d H:i:s")."')";
+			VALUES('".$array["FK_ports"]."','".$array["value"]."','".$array["device_type"]."','".$array["device_ID"]."','".date("Y-m-d H:i:s")."')";
+	
 		}
 		$DB->query($query);
 		
-		$netport->getFromDB($port_networking);
-		$device_type = $netport->fields["device_type"];
-		// Récupérer l'adresse MAC
-		$netport->getFromDB($port_networking);
-		$ifmac = $netport->fields["ifmac"];
-		// Récupération de l'id du matériel
-		$mat_ID = $netport->fields["on_device"];
-		// Ajouter en DB
-		if ($status == "remove")
-		{
-			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
-			(FK_ports,old_value,old_device_type,old_device_ID,date_mod)
-			VALUES('".$ID2."','".$ifmac."','".$device_type."','".$mat_ID."','".date("Y-m-d H:i:s")."')";
-		}
-		else if ($status == "make")
-		{
-			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
-			(FK_ports,new_value,new_device_type,new_device_ID,date_mod)
-			VALUES('".$ID2."','".$ifmac."','".$device_type."','".$mat_ID."','".date("Y-m-d H:i:s")."')";
-	
-		}
-		$DB->query($query);
+		return mysql_insert_id();
 	}
-	
-	
-	
-	function showHistory($ID_port)
+
+
+
+
+	/**
+	 * Insert port history with infos changes
+	 *
+	 * @param $status status of port ('make' or 'remove')
+	 * @param $array with values : $array["field"], $array["FK_ports"], $array["value"], $array["device_type"] and $array["device_ID"]
+	 *
+	 * @return ID of inserted line
+	 *
+	**/
+	function insert_infos($status,$array)
 	{
-		global $DB,$LANG,$LANGTRACKER,$INFOFORM_PAGES,$CFG_GLPI;
-
-		$CommonItem = new CommonItem;
-
-		$query = "
-		SELECT * FROM glpi_plugin_tracker_snmp_history
-		
-		WHERE FK_ports='".$ID_port."'
-		
-		ORDER BY date_mod
-		
-		LIMIT 0,30";		
+		global $DB,$CFG_GLPI;
 	
-		$text = "<table class='tab_cadre' cellpadding='5' width='950'>";
-	
-		$text .= "<tr class='tab_bg_1'>";
-		$text .= "<th colspan='8'>";
-		$text .= "Historique";
-		$text .= "</th>";
-		$text .= "</tr>";
-		
-		$text .= "<tr class='tab_bg_1'>";
-		$text .= "<th>".$LANGTRACKER["snmp"][50]."</th>";
-		$text .= "<th>".$LANG["common"][1]."</th>";
-		$text .= "<th>".$LANG["networking"][15]."</th>";
-		$text .= "<th>".$LANG["event"][18]."</th>";
-		$text .= "<th></th>";
-		$text .= "<th></th>";
-		$text .= "<th></th>";
-		$text .= "<th>".$LANG["common"][27]."</th>";
-		$text .= "</tr>";
-		
-		if ( $result=$DB->query($query) )
+		if ($status == "remove")
 		{
-			while ( $data=$DB->fetch_array($result) )
-			{
-				$text .= "<tr class='tab_bg_1'>";
-				
-				if (($data["old_device_ID"] != "0") OR ($data["new_device_ID"] != "0"))
-				{
-					// Connections and disconnections
-					if ($data["old_device_ID"] != "0")
-					{
-						$text .= "<td align='center'>Déconnexion</td>";
-						$CommonItem->getFromDB($data["old_device_type"],$data["old_device_ID"]);
-						$text .= "<td align='center'>".$CommonItem->getLink(1)."</td>";						
-						$text .= "<td align='center'>".$data["old_value"]."</td>";
-					}
-					else if ($data["new_device_ID"] != "0")
-					{
-						$text .= "<td align='center'>Connexion</td>";
-						$CommonItem->getFromDB($data["new_device_type"],$data["new_device_ID"]);
-						$text .= "<td align='center'>".$CommonItem->getLink(1)."</td>";
-						$text .= "<td align='center'>".$data["new_value"]."</td>";
-					}
-					$text .= "<td align='center' colspan='4'></td>";
-					$text .= "<td align='center'>".$data["date_mod"]."</td>";
-
-				}
-				else
-				{
-				
-					// Changes values
-					$text .= "<td align='center' colspan='3'></td>";
-					$text .= "<td align='center'>".$data["Field"]."</td>";
-					$text .= "<td align='center'>".$data["old_value"]."</td>";
-					$text .= "<td align='center'>-></td>";
-					$text .= "<td align='center'>".$data["new_value"]."</td>";
-					$text .= "<td align='center'>".$data["date_mod"]."</td>";
-				
-				
-				}
-				
-				$text .= "</tr>";
-			}
-			
+			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
+			(FK_ports,Field,old_value,old_device_type,old_device_ID,date_mod)
+			VALUES('".$array["FK_ports"]."','".$array["field"]."','".$array["value"]."','".$array["device_type"]."','".$array["device_ID"]."','".date("Y-m-d H:i:s")."')";
+		
 		}
-
-		$text .= "</table>";
-
-		return $text;
+		else if ($status == "make")
+		{
+			$query = "INSERT INTO glpi_plugin_tracker_snmp_history
+			(FK_ports,Field,new_value,new_device_type,new_device_ID,date_mod)
+			VALUES('".$array["FK_ports"]."','".$array["field"]."','".$array["value"]."','".$array["device_type"]."','".$array["device_ID"]."','".date("Y-m-d H:i:s")."')";
+	
+		}
+	
+		$DB->query($query);
+		
+		return mysql_insert_id();
+	
 	}
+
+
+
+
+
+
+
+
+
+	
+	
+
+	
+	
+	
+
 }
 ?>
