4ce54dd65b8e60409e320ff38a26c3259dfa5a21
David Durieux <d.durieux@siprossii.com>
2008-11-12 00:08:27 +0000

    [tracker] Begin to rewrite functions from class file

--- inc/plugin_tracker.snmp.function.php
+++ inc/plugin_tracker.snmp.function.php
@@ -38,7 +38,92 @@ if (!defined('GLPI_ROOT')){
 }
 
 
+/**
+ * Get all networking list ready for SNMP query  
+ *
+ * @return array with ID => IP 
+ *
+**/
+function getNetworkList()
+{
+	global $DB;
+	
+	$NetworksID = array();	
+	
+	$query = "SELECT active_device_state FROM glpi_plugin_tracker_config ";
+	
+	if ( ($result = $DB->query($query)) )
+	{
+		$device_state = $DB->result($result, 0, "active_device_state");
+	}
 
+	$query = "SELECT ID,ifaddr 
+	FROM glpi_networking 
+	WHERE deleted='0' 
+		AND state='".$device_state."' ";
+		
+	if ( $result=$DB->query($query) )
+	{
+		while ( $data=$DB->fetch_array($result) )
+		{
+			$NetworksID[$data["ID"]] = $data["ifaddr"];
+		}
+	}
+
+	return $NetworksID;
+
+}
+	
+
+
+/**
+ * Get and update infos of networking and its ports  
+ *
+ * @param $ArrayListNetworking ID => IP of the network materiel
+ *
+ * @return nothing
+ *
+**/
+function UpdateNetworkBySNMP($ArrayListNetworking)
+{
+	foreach ( $ArrayListNetworking as $IDNetworking=>$ifIP )
+	{
+		$updateNetwork = new plugin_tracker_snmp;
+		// Get SNMP model 
+		$IDModelInfos = $updateNetwork->GetSNMPModel($IDNetworking);
+		if (($IDModelInfos != "") && ($IDNetworking != ""))
+		{
+			// ** Get oid
+			$ArrayOID = $updateNetwork->GetOID($IDModelInfos);
+			//**
+			$ArrayPortsName = $updateNetwork->GetPortsName($ifIP);
+			//**
+			$ArrayPortsID = $updateNetwork->GetPortsID($IDNetworking);
+			// **
+			$ArrayPortsSNMPNumber = $updateNetwork->GetPortsSNMPNumber($ifIP);
+			// ** Get oid ports Counter
+			$ArrayOIDPorts = $updateNetwork->GetOIDPorts($IDModelInfos,$ifIP,$IDNetworking,$ArrayPortsName,$ArrayPortsSNMPNumber);
+			// ** Define oid and object name
+			$updateNetwork->DefineObject($ArrayOID);
+			// ** Get query SNMP on switch
+			$ArraySNMPResult = $updateNetwork->SNMPQuery($ArrayOID,$ifIP);
+			// ** Define oid and object name
+			//$updateNetwork->DefineObject($ArrayOIDPorts);
+			// ** Get query SNMP of switchs ports
+			$ArraySNMPResultPorts = $updateNetwork->SNMPQuery($ArrayOIDPorts,$ifIP);
+			// ** Get link OID fields
+			$ArrayLinks = $updateNetwork->GetLinkOidToFields($IDModelInfos);
+			// ** Update fields of switchs
+			$updateNetwork->UpdateGLPINetworking($ArraySNMPResult,$ArrayLinks,$IDNetworking);
+			// ** Update ports fields of switchs
+			$updateNetwork->UpdateGLPINetworkingPorts($ArraySNMPResultPorts,$ArrayLinks,$IDNetworking,$ArrayPortsSNMPNumber);
+			
+			// ** Get MAC adress of connected ports
+			$updateNetwork->GetMACtoPort($ifIP,$ArrayPortsID,$IDNetworking);
+		}
+	} 
+
+}
 
 
 ?>
