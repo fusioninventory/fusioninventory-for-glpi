b0aaef6418d8b7bd493e89515d247b12cd1bb020
David Durieux <d.durieux@siprossii.com>
2008-11-12 00:09:11 +0000

    [tracker] Begin to rewrite functions from class file
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@83 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_tracker.snmp.history.function.php
+++ inc/plugin_tracker.snmp.history.function.php
@@ -29,7 +29,7 @@
  */
 
 // ----------------------------------------------------------------------
-// Original Author of file: Nicolas SMOLYNIEC
+// Original Author of file: DURIEUX David
 // Purpose of file:
 // ----------------------------------------------------------------------
 
@@ -38,5 +38,125 @@ if (!defined('GLPI_ROOT')){
 }
 
 
+function addLog()
+{
+	global $DB;
+	
+	
+
+}
+
+
+	
+// $status = connection or disconnection	
+function addLogConnection($status,$port)
+{
+	global $DB,$CFG_GLPI;
+	$CommonItem = new CommonItem;
+	$history = new plugin_tracker_SNMP_history;
+	// Récupérer le port de la machine associé au port du switch
+	$nw=new Netwire;
+	// Récupérer le type de matériel
+	$netport=new Netport;
+	$array["FK_ports"] = $port;
+	$netport->getFromDB($port);
+	$array["device_type"] = $netport->fields["device_type"];
+	
+	// Récupérer l'adresse MAC
+	$netport->getFromDB($port);
+	$array["value"] = $netport->fields["ifmac"];
+	
+	// Récupération de l'id du matériel
+	$array["device_ID"] = $netport->fields["on_device"];
+
+	// Ajouter en DB
+	$history->insert_connection($status,$array);
+}
+
+
+function showHistory($ID_port)
+{
+	global $DB,$LANG,$LANGTRACKER,$INFOFORM_PAGES,$CFG_GLPI;
+
+	$CommonItem = new CommonItem;
+
+	$query = "
+	SELECT * FROM glpi_plugin_tracker_snmp_history
+	
+	WHERE FK_ports='".$ID_port."'
+	
+	ORDER BY date_mod
+	
+	LIMIT 0,30";		
+
+	$text = "<table class='tab_cadre' cellpadding='5' width='950'>";
+
+	$text .= "<tr class='tab_bg_1'>";
+	$text .= "<th colspan='8'>";
+	$text .= "Historique";
+	$text .= "</th>";
+	$text .= "</tr>";
+	
+	$text .= "<tr class='tab_bg_1'>";
+	$text .= "<th>".$LANGTRACKER["snmp"][50]."</th>";
+	$text .= "<th>".$LANG["common"][1]."</th>";
+	$text .= "<th>".$LANG["networking"][15]."</th>";
+	$text .= "<th>".$LANG["event"][18]."</th>";
+	$text .= "<th></th>";
+	$text .= "<th></th>";
+	$text .= "<th></th>";
+	$text .= "<th>".$LANG["common"][27]."</th>";
+	$text .= "</tr>";
+	
+	if ( $result=$DB->query($query) )
+	{
+		while ( $data=$DB->fetch_array($result) )
+		{
+			$text .= "<tr class='tab_bg_1'>";
+			
+			if (($data["old_device_ID"] != "0") OR ($data["new_device_ID"] != "0"))
+			{
+				// Connections and disconnections
+				if ($data["old_device_ID"] != "0")
+				{
+					$text .= "<td align='center'>Déconnexion</td>";
+					$CommonItem->getFromDB($data["old_device_type"],$data["old_device_ID"]);
+					$text .= "<td align='center'>".$CommonItem->getLink(1)."</td>";						
+					$text .= "<td align='center'>".$data["old_value"]."</td>";
+				}
+				else if ($data["new_device_ID"] != "0")
+				{
+					$text .= "<td align='center'>Connexion</td>";
+					$CommonItem->getFromDB($data["new_device_type"],$data["new_device_ID"]);
+					$text .= "<td align='center'>".$CommonItem->getLink(1)."</td>";
+					$text .= "<td align='center'>".$data["new_value"]."</td>";
+				}
+				$text .= "<td align='center' colspan='4'></td>";
+				$text .= "<td align='center'>".$data["date_mod"]."</td>";
+
+			}
+			else
+			{
+			
+				// Changes values
+				$text .= "<td align='center' colspan='3'></td>";
+				$text .= "<td align='center'>".$data["Field"]."</td>";
+				$text .= "<td align='center'>".$data["old_value"]."</td>";
+				$text .= "<td align='center'>-></td>";
+				$text .= "<td align='center'>".$data["new_value"]."</td>";
+				$text .= "<td align='center'>".$data["date_mod"]."</td>";
+			
+			
+			}
+			
+			$text .= "</tr>";
+		}
+		
+	}
+
+	$text .= "</table>";
+
+	return $text;
+}
 
 ?>
