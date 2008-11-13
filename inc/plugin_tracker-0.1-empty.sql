f6bbc393c5f12dc5d6ce682f4aa14543db53392a
David Durieux <d.durieux@siprossii.com>
2008-11-13 21:04:07 +0000

    [tracker] Add ifmac field in tracker port table
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@102 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_tracker-0.1-empty.sql
+++ inc/plugin_tracker-0.1-empty.sql
@@ -159,6 +159,7 @@ CREATE TABLE `glpi_plugin_tracker_networking_ports` (
   `ifoutoctets` int(10) NOT NULL,
   `ifouterrors` int(10) NOT NULL,
   `ifstatus` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ifmac` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=390 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED;
 
