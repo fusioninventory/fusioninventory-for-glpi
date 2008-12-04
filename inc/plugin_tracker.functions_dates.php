2b4ace5390a8ec3673104cbfa1e4307dfdeab5a4
David Durieux <d.durieux@siprossii.com>
2008-12-04 15:54:45 +0000

    [tracker] Specifics dates functions
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@260 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ inc/plugin_tracker.functions_dates.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * @version $Id$
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copynetwork (C) 2003-2006 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: David DURIEUX
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+function plugin_tracker_date ($number_dates,$frequence)
+{
+	$today = time();
+	// ** For weekly stats
+	switch ($frequence)
+	{
+		case "day":
+			// Get days
+			for ($i = 0; $i < $number_dates; $i++)
+			{
+				$dates[] = strftime("%Y-%m-%d", ($today-(86400 * $i)));
+			}
+			break;	
+		case "week":		
+			// Get Last Monday
+			if (gmdate("w", $today) == 0) $today -= (86400 * 6);
+			if (gmdate("w", $today) > 1) $today -= (86400 * (gmdate("w", $today) - 1));
+			$dates[] = strftime("%Y-%m-%d", $today);
+			for ($i = 1; $i < $number_dates; $i++)
+			{
+				$dates[] = strftime("%Y-%m-%d", ($today-((86400 * 7) *  $i)));
+			}
+			break;
+		case "month":	
+			// ** For monthly stats
+			$month = (gmdate("n", $today));
+			$year = (gmdate("Y", $today));
+			$dates[] = strftime("%Y-%m-%d", mktime(0, 0, 0, 1, $month, $year));
+			for ($i = 1; $i < $number_dates; $i++)
+			{
+				$dates[] = strftime("%Y-%m-%d", mktime(0, 0, 0, 1, ($month - $i), $year));
+			}
+			break;
+		case "year":
+			// ** For yearly stats
+			$year = (gmdate("Y", $today));
+			$dates[] = strftime("%Y-%m-%d", mktime(0, 0, 0, 1, 1, $year));
+			for ($i = 1; $i < $number_dates; $i++)
+			{
+				$dates[] = strftime("%Y-%m-%d", mktime(0, 0, 0, 1, 1, ($year - $i)));
+			}
+			break;
+	}
+	return $dates;
+}
+
+?>
