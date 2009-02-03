e3b85b09a471a728d310b11d7762de0658a5df9c
David Durieux <d.durieux@siprossii.com>
2009-02-03 17:51:39 +0000

    [tracker] Corrections
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@599 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_tracker-1.1.0-update.sql
+++ inc/plugin_tracker-1.1.0-update.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `glpi_plugin_tracker_config` ADD `logs2` INT( 1 ) NOT NULL DEFAULT '0';
+ALTER TABLE `glpi_plugin_tracker_config` ADD `logs` INT( 1 ) NOT NULL DEFAULT '0';
 
 ALTER TABLE `glpi_plugin_tracker_networking` ADD `last_tracker_update` DATETIME NULL DEFAULT NULL;
 
