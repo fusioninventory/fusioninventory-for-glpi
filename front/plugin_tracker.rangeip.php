f45034d5b9cdf2739c8d525da023b8d04c00d07c
David Durieux <d.durieux@siprossii.com>
2009-05-17 22:09:34 +0000

    [tracker] Corrections
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@1125 349b9182-4a13-0410-896f-e5e9767dd1b3

--- front/plugin_tracker.rangeip.php
+++ front/plugin_tracker.rangeip.php
@@ -48,8 +48,8 @@ manageGetValuesInSearch(PLUGIN_TRACKER_SNMP_RANGEIP);
 
 $_GET['target']="plugin_tracker.rangeip.php";
 
-searchForm(PLUGIN_TRACKER_SNMP_RANGEIP,$_SERVER['PHP_SELF'],$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["deleted"],$_GET["link"],$_GET["distinct"],$_GET["link2"],$_GET["contains2"],$_GET["field2"],$_GET["type2"]);
-showList(PLUGIN_TRACKER_SNMP_RANGEIP,$_SERVER['PHP_SELF'],$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"],$_GET["distinct"],$_GET["link2"],$_GET["contains2"],$_GET["field2"],$_GET["type2"]);
+searchForm(PLUGIN_TRACKER_SNMP_RANGEIP,$_GET);
+showList(PLUGIN_TRACKER_SNMP_RANGEIP,$_GET);
 
 
 commonFooter();
--- inc/plugin_tracker.snmp.mib.classes.php
+++ inc/plugin_tracker.snmp.mib.classes.php
@@ -37,7 +37,7 @@ if (!defined('GLPI_ROOT'))
 	die("Sorry. You can't access directly to this file");
 
 
-class plugin_tracker_mib_networking extends CommonDBTM
+class plugin_tracker_mib_networking
 {
 	function __construct()
 	{
