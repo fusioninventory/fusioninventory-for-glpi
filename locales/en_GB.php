96050cd4527b5ed89dd370d7c890feb5fc18e792
David Durieux <d.durieux@siprossii.com>
2009-05-17 17:48:51 +0000

    [tracker] Begin 2.1.0 version of Tracker
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@1122 349b9182-4a13-0410-896f-e5e9767dd1b3

--- locales/en_GB.php
+++ locales/en_GB.php
@@ -34,7 +34,7 @@
 // ----------------------------------------------------------------------
 
 $title="Tracker";
-$version="2.0.0";
+$version="2.1.0";
 
 $LANG['tracker']["title"][0]="$title";
 $LANG['tracker']["title"][1]="SNMP information";
