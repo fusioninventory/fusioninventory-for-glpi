c4fb166ac4ff7e7f93c0d42fdc46409ba15b916c
David Durieux <d.durieux@siprossii.com>
2009-06-20 14:12:03 +0000

    [tracker] modification and correction of MySQL table 'glpi_plugin_tracker_unknown_device'
    git-svn-id: https://forge.indepnet.net/svn/tracker/branches/0.71/trunk@1213 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_tracker-2.0.2-update.sql
+++ inc/plugin_tracker-2.0.2-update.sql
@@ -2,7 +2,10 @@ DROP TABLE IF EXISTS `glpi_plugin_tracker_unknown_device`;
 
 CREATE TABLE IF NOT EXISTS `glpi_plugin_tracker_unknown_device` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `date_mod` datetime NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_mod` datetime DEFAULT NULL,
+  `FK_entities` int(11) NOT NULL DEFAULT '0',
+  `location` int(11) NOT NULL DEFAULT '0',
+  `deleted` smallint(6) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
