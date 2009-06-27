de44c9e76f75a8023f52ea6b3a027ab29bfc391b
David Durieux <d.durieux@siprossii.com>
2009-06-27 18:21:49 +0000

    [tracker] Begin modification of unknown devices (unknown mac address) management
    git-svn-id: https://forge.indepnet.net/svn/tracker/trunk@1272 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/plugin_tracker.snmp.history.function.php
+++ inc/plugin_tracker.snmp.history.function.php
@@ -82,6 +82,89 @@ function addLogConnection($status,$port,$FK_process=0)
 }
 
 
+// $status = connection or disconnection
+function plugin_tracker_addLogConnection_unknown_mac($macaddress,$port,$FK_process=0)
+{
+	global $DB,$CFG_GLPI;
+
+	$history = new plugin_tracker_SNMP_history;
+	$netwire = new Netwire;
+	$processes = new Threads;
+
+	// * If glpi device connected to this port, disconnect it
+	$queryVerif = "SELECT *
+		FROM glpi_networking_wire
+		WHERE end1 = '".$port."'
+			OR end2  = '".$port."' ";
+
+	if ($resultVerif=$DB->query($queryVerif))
+	{
+		if ( $DB->numrows($resultVerif) != "0" )
+		{
+			addLogConnection("remove",$netwire->getOppositeContact($port),$FK_process);
+			addLogConnection("remove",$port,$FK_process);
+			while ( $dataVerif2=$DB->fetch_array($resultVerif) )
+			{
+				$query_del = "DELETE FROM glpi_networking_wire
+					WHERE ID='".$dataVerif2["ID"]."' ";
+				$DB->query($query_del);
+			}
+		}
+	}
+
+	// * If other unknown mac adress connected, disconnect it
+	$query = "SELECT last_PID_update FROM glpi_networking_ports
+		LEFT JOIN glpi_networking ON glpi_networking.ID = glpi_networking_ports.on_device
+		LEFT JOIN glpi_plugin_tracker_networking ON glpi_plugin_tracker_networking.FK_networking = glpi_networking.ID
+		WHERE glpi_networking_ports.ID=".$port." ";
+
+	$result = $DB->query($query);
+	$data = $DB->fetch_assoc($result);
+
+	$PID = $data["last_PID_update"];
+echo $PID." => ".$FK_process." => ".$port."\n";
+	list($unknownMac, $unknownIP) = $processes->getUnknownMacFromPIDandPort($PID,$port);
+	if ((!empty($unknownMac)) AND ($unknownMac != $macaddress))
+	{
+		$array["FK_ports"] = $port;
+		$array["value"] = $unknownMac;
+		$array["device_type"] = 0;
+		$array["device_ID"] = 0;
+		$history->insert_connection("remove",$array,$FK_process);
+	}
+
+	// * If this mac address is connected on other port, disconnect it.
+		//$query = "SELECT * FROM glpi_plugin_tracker_unknown_mac
+		// WHERE unknow_mac
+		// AND $FK_process
+
+
+	list($unknownMac_now, $unknownIP_now) = $processes->getUnknownMacFromPIDandPort($FK_process,$port);
+echo $unknownMac." - ".$macaddress."\n";
+	// * If same unknown mac adresse connected, nothing
+	if ($unknownMac == $macaddress)
+	{
+echo "1- Nothing \n";
+	}
+	// else connect in this process
+	elseif ($unknownMac_now == $macaddress)
+	{
+echo "2- Nothing \n";
+	}
+	// else connect it
+	else
+	{
+		echo "3- Connect \n";
+		$array["FK_ports"] = $port;
+		$array["value"] = $macaddress;
+		$array["device_type"] = 0;
+		$array["device_ID"] = 0;
+		$history->insert_connection("make",$array,$FK_process);
+	}
+}
+
+
+// List of history in networking display
 function tracker_snmp_showHistory($ID_port)
 {
 	global $DB,$LANG,$INFOFORM_PAGES,$CFG_GLPI;
@@ -138,6 +221,26 @@ function tracker_snmp_showHistory($ID_port)
 				$text .= "<td align='center' colspan='4'></td>";
 				$text .= "<td align='center'>".convDateTime($data["date_mod"])."</td>";
 
+			}
+			elseif (($data["old_device_ID"] == "0") AND ($data["new_device_ID"] == "0") AND ($data["Field"] == "0"))
+			{
+				// Unknown Mac address
+				if (!empty($data["old_value"]))
+				{
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$LANGTRACKER["history"][2]."</td>";
+					$CommonItem->getFromDB($data["old_device_type"],$data["old_device_ID"]);
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$CommonItem->getLink(1)."</td>";
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$data["old_value"]."</td>";
+				}
+				elseif (!empty($data["new_value"]))
+				{
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$LANGTRACKER["history"][3]."</td>";
+					$CommonItem->getFromDB($data["new_device_type"],$data["new_device_ID"]);
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$CommonItem->getLink(1)."</td>";
+					$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".$data["new_value"]."</td>";
+				}
+				$text .= "<td align='center' colspan='4' background='#cf9b9b' class='tab_bg_1_2'></td>";
+				$text .= "<td align='center' background='#cf9b9b' class='tab_bg_1_2'>".convDateTime($data["date_mod"])."</td>";
 			}
 			else
 			{
