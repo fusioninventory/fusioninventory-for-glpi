8a9f8284fd55363178b8885962b39e1c7dd787c7
David Durieux <d.durieux@siprossii.com>
2010-04-27 10:06:36 +0200

    Set XML encoding in UTF-8

--- front/plugin_fusioninventory.communication.php
+++ front/plugin_fusioninventory.communication.php
@@ -86,7 +86,7 @@ if (!$ptc->import(gzuncompress($GLOBALS["HTTP_RAW_POST_DATA"]))) {
      
       if (isset($pxml->DEVICEID)) {
 
-         $ptc->setXML("<?xml version='1.0' encoding='ISO-8859-1'?>
+         $ptc->setXML("<?xml version='1.0' encoding='UTF-8'?>
 <REPLY>
 </REPLY>");
 
