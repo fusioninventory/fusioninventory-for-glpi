bdee5fdef5fce211140a0552e1524d237dd8c6d3
David Durieux <d.durieux@siprossii.com>
2010-05-13 23:42:48 +0200

    Fix sql error on device queried not found object in GLPI MySQL

--- inc/communication.classes.php
+++ inc/communication.classes.php
@@ -723,18 +723,20 @@ class PluginFusionInventoryCommunication {
                $ptae->addError($a_input);
             }
          } else {
-            $ptap->updateProcess($_SESSION['glpi_plugin_fusioninventory_processnumber'],
-                  array('query_nb_error' => '1'));
-            $a_input = array();
-            $a_input['ID'] = $p_device->ERROR->ID;
-            if ($p_device->ERROR->TYPE=='NETWORKING') {
-               $a_input['TYPE'] = NETWORKING_TYPE;
-            } elseif ($p_device->ERROR->TYPE=='PRINTER') {
-               $a_input['TYPE'] = PRINTER_TYPE;
+            if ($errors != '') {
+               $ptap->updateProcess($_SESSION['glpi_plugin_fusioninventory_processnumber'],
+                     array('query_nb_error' => '1'));
+               $a_input = array();
+               $a_input['ID'] = $p_device->ERROR->ID;
+               if ($p_device->ERROR->TYPE=='NETWORKING') {
+                  $a_input['TYPE'] = NETWORKING_TYPE;
+               } elseif ($p_device->ERROR->TYPE=='PRINTER') {
+                  $a_input['TYPE'] = PRINTER_TYPE;
+               }
+               $a_input['MESSAGE'] = $errors;
+               $a_input['agent_type'] = 'SNMPQUERY';
+               $ptae->addError($a_input);
             }
-            $a_input['MESSAGE'] = $errors;
-            $a_input['agent_type'] = 'SNMPQUERY';
-            $ptae->addError($a_input);
          }
       }
 
@@ -815,6 +817,7 @@ class PluginFusionInventoryCommunication {
          $a_input['MESSAGE'] = $errors;
          $a_input['agent_type'] = 'SNMPQUERY';
          $pfiae->addError($a_input);
+         $errors = '';
       }
 
       return $errors;
