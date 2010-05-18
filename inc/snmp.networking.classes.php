fca8e302be8e27b978881d42c000c29d5ddf5c4b
David Durieux <d.durieux@siprossii.com>
2010-05-18 23:18:33 +0200

    Fix javascript to select fields in networking ports in tab fusioninventory

--- inc/snmp.networking.classes.php
+++ inc/snmp.networking.classes.php
@@ -417,10 +417,7 @@ function appear_array(id){
 		echo '<th><img alt="Sélectionnez les éléments à afficher par défaut" 
                      title="Sélectionnez les éléments à afficher par défaut"
                      src="'.GLPI_ROOT.'/pics/options_search.png" class="pointer"
-                     onclick="var w = window.open(\''.GLPI_ROOT.
-                        '/front/popup.php?popup=search_config&type=5157\' ,\'glpipopup\',
-                        \'height=400,
-                     width=1000, top=100, left=100, scrollbars=yes\' ); w.focus();"></th>';
+                     onclick="var w = window.open(\''.GLPI_ROOT.'/front/popup.php?popup=search_config&type=5157\' ,\'glpipopup\', \'height=400, width=1000, top=100, left=100, scrollbars=yes\' ); w.focus();"></th>';
 		echo "<th>".$LANG["common"][16]."</th>";
 
 		$query_array = "SELECT * 
