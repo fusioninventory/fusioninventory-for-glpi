056cc25238221e06afab3979bc37dee6bda1d0d1
Vincent MAZZONI <v.mazzoni@siprossii.com>
2010-07-02 16:48:42 +0200

    Continue split communication class

---
+++ inc/commondbtm.class.php
@@ -0,0 +1,189 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: MAZZONI Vincent
+// Purpose of file: modelisation of multi-tables objects (switch, printer...)
+// ----------------------------------------------------------------------
+
+if (!defined('GLPI_ROOT')) {
+	die("Sorry. You can't access directly to this file");
+}
+
+/**
+ * Class to manage generic multi-tables objects
+ * Adds field management to core CommonDBTM class.
+ **/
+class PluginFusinvsnmpCommonDBTM extends CommonDBTM {
+   private $ptcdFields=array();
+   private $ptcdLockFields=array();
+   protected $ptcdUpdates=array();
+   protected $ptcdLinkedObjects=array();
+
+	/**
+	 * Constructor
+	**/
+   function __construct($p_table) {
+      $this->table=$p_table;
+   }
+
+   /**
+    * Load an existing item
+    *
+    *@return nothing
+    **/
+   function load($p_id='') {
+      global $DB, $LINK_ID_TABLE;
+
+      if ($p_id!='') { // existing item : load old values
+         $this->getFromDB($p_id);
+         $this->ptcdFields=$this->fields;
+         $itemtype=array_search($this->table, $LINK_ID_TABLE);
+         if ($itemtype) {
+            $this->ptcdLockFields=PluginFusioninventoryLock::getLockFields($itemtype, $p_id);
+         }
+      } else { // new item : initialize all fields to NULL
+         $query = "SHOW COLUMNS FROM `".$this->table."`";
+         if ($result=$DB->query($query)) {
+            while ($data=$DB->fetch_array($result)) {
+               $this->ptcdFields[$data[0]]=NULL;
+            }
+         }
+      }
+   }
+
+   /**
+    * Add a new item with the instance values
+    *
+    *@param $p_force=FALSE Force add even if no updates where done
+    *@return nothing
+    **/
+   function addCommon($p_force=FALSE) {
+      if (count($this->ptcdUpdates) OR $p_force) {
+         $itemID=parent::add($this->ptcdUpdates);
+         $this->load($itemID);
+      }
+   }
+
+   /**
+    * Update an existing preloaded item with the instance values or add a new one
+    *
+    *@return nothing
+    **/
+   function updateDB() {
+      if (count($this->ptcdUpdates)) {
+         $this->ptcdUpdates['id'] = $this->getValue('id');
+         if ($this->ptcdUpdates['id'] != '') {
+            $this->update($this->ptcdUpdates);
+         } else {
+            unset($this->ptcdUpdates['id']);
+            $this->add($this->ptcdUpdates);
+         }
+      }
+   }
+
+   /**
+    * Delete a loaded item
+    *
+    *@param $p_id Item id
+    *@return nothing
+    **/
+   function deleteDB() {
+      $this->deleteFromDB($this->ptcdFields['id'], 1);
+   }
+
+   /**
+    * Get all objetc vars and values
+    *
+    *@return Array of all class vars => values
+    **/
+   function getVars() {
+      return get_object_vars($this);
+   }
+
+   /**
+    * Get field value
+    *
+    *@param $p_field field
+    *@param $p_object=NULL Object to update
+    *@return field value / NULL if unknown field
+    **/
+   function getValue($p_field, $p_object=NULL) {
+      if (is_null($p_object)) {
+         $p_object=$this;
+      }
+      if (array_key_exists($p_field, $p_object->ptcdFields)) {
+         return $p_object->ptcdFields[$p_field];
+      } else {
+         foreach ($p_object->ptcdLinkedObjects as $object) {
+            $value = $object->getValue($p_field, $object);
+            if (!is_null($value)) {
+               return $value;
+            }
+         }
+         return NULL;
+      }
+   }
+
+   /**
+    * Set field value
+    *
+    *@param $p_field field
+    *@param $p_value Value
+    *@param $p_object=NULL Object to update
+    *@return true if value set / false if unknown field
+    **/
+   function setValue($p_field, $p_value, $p_object=NULL, $p_default='') {
+      // TODO : replace $p_default by check default value in DB ?
+      if (is_null($p_object)) {
+         $p_object=$this;
+      }
+      if (array_key_exists($p_field, $p_object->ptcdFields)) {
+         if (!in_array($p_field, $this->ptcdLockFields)) { // don't update if field is locked
+            if ($p_object->ptcdFields[$p_field]!=$p_value) { // don't update if values are the same
+               if (!($p_object->getValue($p_field)==$p_default AND $p_value=="")) { // don't update if both values are empty
+                  $p_object->ptcdFields[$p_field] = $p_value;
+                  $p_object->ptcdUpdates[$p_field] = $p_value;
+               }
+            }
+         }
+         return true;
+      } else {
+         foreach ($this->ptcdLinkedObjects as $object) {
+            if ($object->setValue($p_field, $p_value)) {
+               return true;
+            }
+         }
+         return false;
+      }
+   }
+}
+
+?>
---
+++ inc/communicationsnmp.class.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: MAZZONI Vincent
+// Purpose of file: management of snmp communication with agents
+// ----------------------------------------------------------------------
+/**
+ * The datas are XML encoded and compressed with Zlib.
+ * XML rules :
+ * - XML tags in uppercase
+ **/
+
+if (!defined('GLPI_ROOT')) {
+	die("Sorry. You can't access directly to this file");
+}
+
+/**
+ * Class to communicate with agents using XML
+ **/
+class PluginFusinvsnmpCommunicationSNMP {
+   private $sxml, $deviceId;
+
+   /**
+    * Add AUTHENTICATION string to XML node
+    *
+    *@param $p_sxml_node XML node to authenticate
+    *@param $p_id Authenticate id
+    *@return nothing
+    **/
+   function addAuth($p_sxml_node, $p_id) {
+      $ptsnmpa = new PluginFusioninventoryConfigSNMPSecurity;
+      $ptsnmpa->getFromDB($p_id);
+
+      $sxml_authentication = $p_sxml_node->addChild('AUTHENTICATION');
+         $sxml_authentication->addAttribute('ID', $p_id);
+         $sxml_authentication->addAttribute('COMMUNITY', $ptsnmpa->fields['community']);
+         $sxml_authentication->addAttribute('VERSION',
+                            Dropdown::getDropdownName('glpi_plugin_fusioninventory_snmpversions',
+                                            $ptsnmpa->fields['plugin_fusioninventory_snmpversions_id']));
+         $sxml_authentication->addAttribute('USERNAME', $ptsnmpa->fields['username']);
+         if ($ptsnmpa->fields['authentication'] == '0') {
+            $sxml_authentication->addAttribute('AUTHPROTOCOL', '');
+         } else {
+            $sxml_authentication->addAttribute('AUTHPROTOCOL',
+                            Dropdown::getDropdownName('glpi_plugin_fusioninventory_snmpprotocolauths',
+                                            $ptsnmpa->fields['authentication']));
+         }
+         $sxml_authentication->addAttribute('AUTHPASSPHRASE', $ptsnmpa->fields['auth_passphrase']);
+         if ($ptsnmpa->fields['encryption'] == '0') {
+            $sxml_authentication->addAttribute('PRIVPROTOCOL', '');
+         } else {
+            $sxml_authentication->addAttribute('PRIVPROTOCOL',
+                    Dropdown::getDropdownName('glpi_plugin_fusioninventory_snmpprotocolprivs',
+                                    $ptsnmpa->fields['encryption']));
+         }
+         $sxml_authentication->addAttribute('PRIVPASSPHRASE', $ptsnmpa->fields['priv_passphrase']);
+   }
+
+   /**
+    * Set XML code
+    *
+    *@param $p_xml XML code
+    *@return nothing
+    **/
+   function setXML($p_xml) {
+      $this->sxml = @simplexml_load_string($p_xml); // @ to avoid xml warnings
+   }
+
+   
+   function addProcessNumber($p_pid) {
+      $this->sxml->addChild('PROCESSNUMBER', $p_pid);
+      //var_dump($this->sxml);
+   }
+}
+
+?>
