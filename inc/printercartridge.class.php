15b06aa2d15cd0359b1b2b616c3c55b57c9ca2ba
David Durieux <d.durieux@siprossii.com>
2010-09-16 12:34:56 +0200

    Clean

--- inc/printercartridge.class.php
+++ inc/printercartridge.class.php
@@ -41,138 +41,133 @@ if (!defined('GLPI_ROOT')) {
 class PluginFusinvsnmpPrinterCartridge extends PluginFusinvsnmpCommonDBTM {
    
 	function __construct() {
-      //parent::__construct("glpi_plugin_fusinvsnmp_printercartridges");
       $this->table = "glpi_plugin_fusinvsnmp_printercartridges";
       $this->type = 'PluginFusinvsnmpPrinterCartridge';
 	}
 
 
-      function showForm($id, $options=array()) {
-         global $LANG;
+   function showForm($id, $options=array()) {
+      global $LANG;
 
-         $plugin_fusioninventory_snmp = new PluginFusinvsnmpSNMP;
+      $plugin_fusioninventory_snmp = new PluginFusinvsnmpSNMP;
 
-         // get infos to get visible or not the counters
-			$snmp_model_ID = $plugin_fusioninventory_snmp->GetSNMPModel($id, PRINTER_TYPE);
-			// ** Get link OID fields
-			$Array_Object_TypeNameConstant= $plugin_fusioninventory_snmp->GetLinkOidToFields($id, PRINTER_TYPE);
-			$mapping_name=array();
-			foreach ($Array_Object_TypeNameConstant as $object=>$mapping_type_name) {
-				if ((strstr($mapping_type_name, "cartridge")) OR (strstr($mapping_type_name, "toner"))) {
-               switch($mapping_type_name) {
-                     CASE "cartridgeblack":
-                        $mapping_name[$mapping_type_name] = "1";
-                        break;
+      // get infos to get visible or not the counters
+      $snmp_model_ID = $plugin_fusioninventory_snmp->GetSNMPModel($id, PRINTER_TYPE);
+      // ** Get link OID fields
+      $Array_Object_TypeNameConstant= $plugin_fusioninventory_snmp->GetLinkOidToFields($id, PRINTER_TYPE);
+      $mapping_name=array();
+      foreach ($Array_Object_TypeNameConstant as $object=>$mapping_type_name) {
+         if ((strstr($mapping_type_name, "cartridge")) OR (strstr($mapping_type_name, "toner"))) {
+            switch($mapping_type_name) {
+                  CASE "cartridgeblack":
+                     $mapping_name[$mapping_type_name] = "1";
+                     break;
 
-                     CASE "cartridgeblackphoto":
-                        $mapping_name[$mapping_type_name] = "2";
-                        break;
+                  CASE "cartridgeblackphoto":
+                     $mapping_name[$mapping_type_name] = "2";
+                     break;
 
-                     CASE "tonerblack" :
-                        $mapping_name[$mapping_type_name] = "3";
-                        break;
+                  CASE "tonerblack" :
+                     $mapping_name[$mapping_type_name] = "3";
+                     break;
 
-                     CASE "tonerblack2" :
-                        $mapping_name[$mapping_type_name] = "4";
-                        break;
+                  CASE "tonerblack2" :
+                     $mapping_name[$mapping_type_name] = "4";
+                     break;
 
-                     CASE "cartridgecyan":
-                        $mapping_name[$mapping_type_name] = "5";
-                        break;
+                  CASE "cartridgecyan":
+                     $mapping_name[$mapping_type_name] = "5";
+                     break;
 
-                     CASE "cartridgecyanlight":
-                        $mapping_name[$mapping_type_name] = "6";
-                        break;
+                  CASE "cartridgecyanlight":
+                     $mapping_name[$mapping_type_name] = "6";
+                     break;
 
-                     CASE "tonercyan" :
-                        $mapping_name[$mapping_type_name] = "7";
-                        break;
+                  CASE "tonercyan" :
+                     $mapping_name[$mapping_type_name] = "7";
+                     break;
 
-                     CASE "cartridgemagenta":
-                        $mapping_name[$mapping_type_name] = "8";
-                        break;
+                  CASE "cartridgemagenta":
+                     $mapping_name[$mapping_type_name] = "8";
+                     break;
 
-                     CASE "cartridgemagentalight":
-                        $mapping_name[$mapping_type_name] = "9";
-                        break;
+                  CASE "cartridgemagentalight":
+                     $mapping_name[$mapping_type_name] = "9";
+                     break;
 
-                     CASE "tonermagenta":
-                        $mapping_name[$mapping_type_name] = "10";
-                        break;
+                  CASE "tonermagenta":
+                     $mapping_name[$mapping_type_name] = "10";
+                     break;
 
-                     CASE "cartridgeyellow":
-                        $mapping_name[$mapping_type_name] = "11";
-                        break;
+                  CASE "cartridgeyellow":
+                     $mapping_name[$mapping_type_name] = "11";
+                     break;
 
-                     CASE "toneryellow":
-                        $mapping_name[$mapping_type_name] = "12";
-                        break;
+                  CASE "toneryellow":
+                     $mapping_name[$mapping_type_name] = "12";
+                     break;
 
-                     CASE "drumblack":
-                        $mapping_name[$mapping_type_name] = "13";
-                        break;
+                  CASE "drumblack":
+                     $mapping_name[$mapping_type_name] = "13";
+                     break;
 
-                     CASE "drumcyan":
-                        $mapping_name[$mapping_type_name] = "14";
-                        break;
+                  CASE "drumcyan":
+                     $mapping_name[$mapping_type_name] = "14";
+                     break;
 
-                     CASE "drummagenta":
-                        $mapping_name[$mapping_type_name] = "15";
-                        break;
+                  CASE "drummagenta":
+                     $mapping_name[$mapping_type_name] = "15";
+                     break;
 
-                     CASE "drumyellow":
-                        $mapping_name[$mapping_type_name] = "16";
-                        break;
+                  CASE "drumyellow":
+                     $mapping_name[$mapping_type_name] = "16";
+                     break;
 
-                     CASE "wastetoner":
-                        $mapping_name[$mapping_type_name] = "17";
-                        break;
+                  CASE "wastetoner":
+                     $mapping_name[$mapping_type_name] = "17";
+                     break;
 
-                     CASE "maintenancekit":
-                        $mapping_name[$mapping_type_name] = "18";
-                        break;
+                  CASE "maintenancekit":
+                     $mapping_name[$mapping_type_name] = "18";
+                     break;
 
-                     default:
-                        $mapping_name[$mapping_type_name] = "19";
-               }
-				}
-			}
-         echo "<br/>";
-         echo "<div align='center'><form method='post' name='snmp_form' id='snmp_form'
-                    action=\"".$options['target']."\">";
-         echo "<table class='tab_cadre' cellpadding='5' width='950'>";
-         echo "<tr class='tab_bg_1'>";
-         echo "<th align='center' colspan='3'>";
-         echo $LANG["cartridges"][16];
-         echo "</th>";
-         echo "</tr>";
-
-         asort($mapping_name);
-         $mapping = new PluginFusioninventoryMapping();
-         foreach ($mapping_name as $cartridge_name=>$val) {
-            $state = $this->cartridges_state($id, $cartridge_name);
-            echo "<tr class='tab_bg_1'>";
-            echo "<td align='center'>";
-   //			echo $FUSIONINVENTORY_MAPPING[PRINTER_TYPE][$cartridge_name]['shortname'];
-            $mapfields = $mapping->get('Printer', $cartridge_name);
-            if ($mapfields != false) {
-   //            echo $FUSIONINVENTORY_MAPPING[PRINTER_TYPE][$cartridge_name]['shortname'];
-               echo $LANG['plugin_fusioninventory']["mapping"][$mapfields['shortlocale']];
+                  default:
+                     $mapping_name[$mapping_type_name] = "19";
             }
-            echo " : ";
-            echo "</td>";
-            echo "<td align='center'>";
-            echo "</td>";
-            echo "<td align='center'>";
-            PluginFusioninventoryDisplay::bar($state['state']);
-            echo "</td>";
-            echo "</tr>";
          }
-
-         echo "</table></form>";
-         echo "</div>";
-
       }
+      echo "<br/>";
+      echo "<div align='center'><form method='post' name='snmp_form' id='snmp_form'
+                 action=\"".$options['target']."\">";
+      echo "<table class='tab_cadre' cellpadding='5' width='950'>";
+      echo "<tr class='tab_bg_1'>";
+      echo "<th align='center' colspan='3'>";
+      echo $LANG["cartridges"][16];
+      echo "</th>";
+      echo "</tr>";
+
+      asort($mapping_name);
+      $mapping = new PluginFusioninventoryMapping();
+      foreach ($mapping_name as $cartridge_name=>$val) {
+         $state = $this->cartridges_state($id, $cartridge_name);
+         echo "<tr class='tab_bg_1'>";
+         echo "<td align='center'>";
+         $mapfields = $mapping->get('Printer', $cartridge_name);
+         if ($mapfields != false) {
+            echo $LANG['plugin_fusioninventory']["mapping"][$mapfields['shortlocale']];
+         }
+         echo " : ";
+         echo "</td>";
+         echo "<td align='center'>";
+         echo "</td>";
+         echo "<td align='center'>";
+         PluginFusioninventoryDisplay::bar($state['state']);
+         echo "</td>";
+         echo "</tr>";
+      }
+      echo "</table></form>";
+      echo "</div>";
+   }
 
 
    function cartridges_state($FK_printers, $object_name) {
