749f6ffa9d187014676a7b2619afd84cc9a1b96a
David Durieux <d.durieux@siprossii.com>
2010-09-30 10:32:41 +0200

    Add errors code from ocsdeploy module

--- inc/communicationocsdeploy.class.php
+++ inc/communicationocsdeploy.class.php
@@ -58,29 +58,100 @@ class PluginFusinvdeployCommunicationOcsdeploy extends PluginFusinvsnmpCommunica
     *@param $p_CONTENT XML code to import
     *@return "" (import ok) / error string (import ko)
     **/
-   function import($p_DEVICEID, $p_CONTENT) {
-      global $LANG;
+   function import($p_DEVICEID, $p_CONTENT, $p_xml) {
+      global $DB, $LANG;
 
       $PluginFusioninventoryTaskjobstatus = new PluginFusioninventoryTaskjobstatus;
       $PluginFusioninventoryAgent = new PluginFusioninventoryAgent;
 
       PluginFusioninventoryCommunication::addLog(
               'Function PluginFusinvdeployCommunicationOcsdeploy->import().');
-      $this->setXML($p_CONTENT);
+//      $this->setXML($p_CONTENT);
+      $this->sxml = @simplexml_load_string($p_xml);
       $errors = '';
 
-      if (isset($p_CONTENT->ERR)) {
-         $errors = $p_CONTENT->ERR;
+      if (isset($this->sxml->ERR)) {
+         $errors = $this->sxml->ERR;
       }
 
-      // Suivant les erreurs
-         // get JOB id with package ID ($p_CONTENT->ID) and agent id
-      $agent = $PluginFusioninventoryAgent->InfosByKey($p_CONTENT->DEVICEID);
+      $agent = $PluginFusioninventoryAgent->InfosByKey($p_DEVICEID);
+
+      $query = "SELECT `glpi_plugin_fusioninventory_taskjobstatus`.* FROM `glpi_plugin_fusioninventory_taskjobstatus`
+               LEFT JOIN `glpi_plugin_fusioninventory_taskjobs`
+                  on `plugin_fusioninventory_taskjobs_id` = `glpi_plugin_fusioninventory_taskjobs`.`id`
+
+               WHERE `glpi_plugin_fusioninventory_taskjobstatus`.`plugin_fusioninventory_agents_id`='".$agent['id']."'
+                  AND `itemtype`='Computer'
+                  AND `state`='0'
+                  AND `method`='ocsdeploy'
+                  AND `argument`='".$this->sxml->ID."'
+               LIMIT 1"; //TODO : state search for 1 and not 0
+      if ($result = $DB->query($query)) {
+         if ($DB->numrows($result) != 0) {
+            $a_taskjobstatus = $DB->fetch_assoc($result);
+
+
+            $is_error = 0;
+            if ($errors != '') {
+               $is_error = 1;
+
+               /*
+                * Errors messages
+                *
+                * ERR_CLEAN
+                * ERR_EXECUTE
+                * ERR_DOWNLOAD_PACK
+                * ERR_BUILD
+                * ERR_BAD_DIGEST
+                * ERR_ALREADY_SETUP
+                * ERR_DOWNLOAD_INFO
+                */
+
+               switch($errors) {
+
+                  case 'ERR_CLEAN':
+                     $errors .= " : fail to clean fiels of package";
+                     break;
+
+                  case 'ERR_EXECUTE':
+                     $errors .= " : fail to uncompress or to run package";
+                     break;
+
+                  case 'ERR_DOWNLOAD_PACK':
+                     $errors .= " : unable to download fragments of package";
+                     break;
+
+                  case 'ERR_BUILD';
+                     // TODO : ???
+                     break;
+
+                  case 'ERR_BAD_DIGEST':
+                     // TODO : ???
+                     break;
+
+                  case 'ERR_ALREADY_SETUP':
+                     $errors .= ' : this package is yet installed';
+                     break;
+
+                  case 'ERR_DOWNLOAD_INFO':
+                     $errors .= 'Unable to contact server and get informations of package';
+                     break;
+                  
+               }
+            }
+
+            // Update taskjobstatus (state = 3 : finish);
+            $PluginFusioninventoryTaskjobstatus->changeStatusFinish(
+                           $a_taskjobstatus['plugin_fusioninventory_taskjobs_id'],
+                           $a_taskjobstatus['items_id'],
+                           'Computer',
+                           $is_error,
+                           $errors,
+                           0);
+
+         }
+      }
       
-
-      // Update taskjobstatus (state = 3 : finish); 
-      //$PluginFusioninventoryTaskjobstatus->changeStatusFinish($taskjobs_id, $items_id, $itemtype, 0, $p_CONTENT->ERR, 1);
-
 
       return $errors;
    }
