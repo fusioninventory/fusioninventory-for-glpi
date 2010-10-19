a4676b66e271a50a03d5e83b049ae23c92cd8d07
David Durieux <d.durieux@siprossii.com>
2010-10-20 00:39:07 +0200

    Add test for snmpinventory

--- tools/phpunit/test.php
+++ tools/phpunit/test.php
@@ -19,16 +19,12 @@ function prolog() {
 
 
 $emulatorAgent = new emulatorAgent;
-$emulatorAgent->server_urlpath = "/glpi072/plugins/fusioninventory/front/plugin_fusioninventory.communication.php";
+$emulatorAgent->server_urlpath = "/glpi078/plugins/fusioninventory/front/communication.php";
 //$emulatorAgent->Start('192.168.1.14','62354', 'prolog');
 
+   $input_xml = file_get_contents("xml/inventory_snmp/1.2/cisco2960.xml");
 
-   $input = '<?xml version="1.0" encoding="UTF-8"?>
-<REQUEST>
-  <DEVICEID>port003-2010-06-08-08-13-45</DEVICEID>
-  <QUERY>PROLOG</QUERY>
-  <TOKEN>CBXTMXLU</TOKEN>
-</REQUEST>';
-   $emulatorAgent->sendProlog($input);
+   $return = $emulatorAgent->sendProlog($input_xml);
+   print_r($return);
 
 ?>
