b12734eb35b09153b3b25f767965650f414e4a38
Vincent MAZZONI <v.mazzoni@siprossii.com>
2010-10-29 15:28:50 +0200

    Check rights

--- front/menu.php
+++ front/menu.php
@@ -37,9 +37,15 @@ if (!defined('GLPI_ROOT')) {
 
 include (GLPI_ROOT."/inc/includes.php");
 
-commonHeader($LANG['plugin_fusioninventory']["title"][0],$_SERVER["PHP_SELF"],"plugins","fusioninventory", "menu");
+if (PluginFusioninventoryProfile::haveRight("fusioninventory", "agents", "r")
+      OR PluginFusioninventoryProfile::haveRight("fusioninventory", "remotecontrol","r")
+      ) {
+   commonHeader($LANG['plugin_fusioninventory']["title"][0],$_SERVER["PHP_SELF"],"plugins","fusioninventory", "menu");
 
-PluginFusioninventoryMenu::displayMenu();
+   PluginFusioninventoryMenu::displayMenu();
+} else {
+	displayRightError();
+}
 
 commonFooter();
 
