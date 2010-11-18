d721c68f356f56c94dc792298b70b756d90f7634
David Durieux <d.durieux@siprossii.com>
2010-11-18 10:44:22 +0100

    Add getSearchOptions of agent

--- inc/agent.class.php
+++ inc/agent.class.php
@@ -74,6 +74,59 @@ class PluginFusioninventoryAgent extends CommonDBTM {
    }
 
 
+   function getSearchOptions() {
+      global $LANG;
+
+      $tab = array();
+    
+      $tab['common'] = $LANG['plugin_fusioninventory']["agents"][28];
+
+		$tab[1]['table'] = $this->getTable();
+		$tab[1]['field'] = 'name';
+		$tab[1]['linkfield'] = 'name';
+		$tab[1]['name'] = $LANG['common'][16];
+		$tab[1]['datatype'] = 'itemlink';
+
+		$tab[2]['table'] = $this->getTable();
+		$tab[2]['field'] = 'last_contact';
+		$tab[2]['linkfield'] = 'last_contact';
+		$tab[2]['name'] = $LANG['plugin_fusioninventory']["agents"][4];
+		$tab[2]['datatype'] = 'datetime';
+
+		$tab[3]['table'] = $this->getTable();
+		$tab[3]['field'] = 'lock';
+		$tab[3]['linkfield'] = 'lock';
+		$tab[3]['name'] = $LANG['plugin_fusioninventory']["agents"][6];
+		$tab[3]['datatype'] = 'bool';
+
+		$tab[4]['table'] = $this->getTable();
+		$tab[4]['field'] = 'device_id';
+		$tab[4]['linkfield'] = 'device_id';
+		$tab[4]['name'] = 'Device_id';
+		$tab[4]['datatype'] = 'text';
+
+		$tab[5]['table'] = 'glpi_computers';
+		$tab[5]['field'] = 'name';
+		$tab[5]['linkfield'] = 'items_id';
+		$tab[5]['name'] = $LANG['plugin_fusioninventory']["agents"][23];
+		$tab[5]['datatype'] = 'itemlink';
+
+		$tab[6]['table'] = $this->getTable();
+		$tab[6]['field'] = 'version';
+		$tab[6]['linkfield'] = 'version';
+		$tab[6]['name'] = $LANG['plugin_fusioninventory']["agents"][25];
+		$tab[6]['datatype'] = 'text';
+
+		$tab[7]['table'] = $this->getTable();
+		$tab[7]['field'] = 'token';
+		$tab[7]['linkfield'] = 'token';
+		$tab[7]['name'] = $LANG['plugin_fusioninventory']["agents"][24];
+		$tab[7]['datatype'] = 'text';
+
+      return $tab;
+   }
+
+
 
    function defineTabs($options=array()){
       global $LANG,$CFG_GLPI;
