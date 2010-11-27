b4ed15e7b8364eaa9c080b12dba7a0f0d5a37735
David Durieux <d.durieux@siprossii.com>
2010-11-27 12:49:31 +0100

    Coding standard

--- locales/en_GB.php
+++ locales/en_GB.php
@@ -36,26 +36,26 @@
 $title="FusionInventory INVENTORY";
 $version="2.3.0-1";
 
-$LANG['plugin_fusinvinventory']["title"][0]="$title";
+$LANG['plugin_fusinvinventory']['title'][0]="$title";
 
-$LANG['plugin_fusinvinventory']["setup"][17]="Plugin ".$title." need plugin FusionInventory activated before activation.";
-$LANG['plugin_fusinvinventory']["setup"][18]="Plugin ".$title." need plugin FusionInventory activated before uninstall.";
-$LANG['plugin_fusinvinventory']["setup"][20]="Import options";
-$LANG['plugin_fusinvinventory']["setup"][21]="Components";
-$LANG['plugin_fusinvinventory']["setup"][22]="Global import";
-$LANG['plugin_fusinvinventory']["setup"][23]="No import";
-$LANG['plugin_fusinvinventory']["setup"][24]="Unique import";
-$LANG['plugin_fusinvinventory']["setup"][25]="Registry";
-$LANG['plugin_fusinvinventory']["setup"][26]="Processus";
-$LANG['plugin_fusinvinventory']["setup"][27]="Unique import on serial number";
+$LANG['plugin_fusinvinventory']['setup'][17]="Plugin ".$title." need plugin FusionInventory activated before activation.";
+$LANG['plugin_fusinvinventory']['setup'][18]="Plugin ".$title." need plugin FusionInventory activated before uninstall.";
+$LANG['plugin_fusinvinventory']['setup'][20]="Import options";
+$LANG['plugin_fusinvinventory']['setup'][21]="Components";
+$LANG['plugin_fusinvinventory']['setup'][22]="Global import";
+$LANG['plugin_fusinvinventory']['setup'][23]="No import";
+$LANG['plugin_fusinvinventory']['setup'][24]="Unique import";
+$LANG['plugin_fusinvinventory']['setup'][25]="Registry";
+$LANG['plugin_fusinvinventory']['setup'][26]="Processus";
+$LANG['plugin_fusinvinventory']['setup'][27]="Unique import on serial number";
 
-$LANG['plugin_fusinvinventory']["menu"][0]="Import agent XML file";
-$LANG['plugin_fusinvinventory']["menu"][1]="Criteria rules";
-$LANG['plugin_fusinvinventory']["menu"][2]="BlackList";
-$LANG['plugin_fusinvinventory']["menu"][3]="Entity rules";
+$LANG['plugin_fusinvinventory']['menu'][0]="Import agent XML file";
+$LANG['plugin_fusinvinventory']['menu'][1]="Criteria rules";
+$LANG['plugin_fusinvinventory']['menu'][2]="BlackList";
+$LANG['plugin_fusinvinventory']['menu'][3]="Entity rules";
 
-$LANG['plugin_fusinvinventory']["importxml"][0]="Import Agent XML file";
-$LANG['plugin_fusinvinventory']["importxml"][1]="Computer injected into GLPI";
+$LANG['plugin_fusinvinventory']['importxml'][0]="Import Agent XML file";
+$LANG['plugin_fusinvinventory']['importxml'][1]="Computer injected into GLPI";
 
 $LANG['plugin_fusinvinventory']['rule'][0]="Computer existent criteria rules";
 $LANG['plugin_fusinvinventory']['rule'][1]="Existant criterium";
@@ -73,10 +73,10 @@ $LANG['plugin_fusinvinventory']['rule'][31]="Import in unknown devices";
 $LANG['plugin_fusinvinventory']['rule'][100]="Entity rules";
 $LANG['plugin_fusinvinventory']['rule'][101]="Tag";
 
-$LANG['plugin_fusinvinventory']["xml"][0]="XML FusionInventory";
+$LANG['plugin_fusinvinventory']['xml'][0]="XML FusionInventory";
 
-$LANG['plugin_fusinvinventory']["blacklist"][0]="blacklisted values";
-$LANG['plugin_fusinvinventory']["blacklist"][1]="New value to blacklist";
+$LANG['plugin_fusinvinventory']['blacklist'][0]="blacklisted values";
+$LANG['plugin_fusinvinventory']['blacklist'][1]="New value to blacklist";
 
 $LANG['plugin_fusinvinventory']['profile'][1]="$title";
 $LANG['plugin_fusinvinventory']['profile'][2]="Existent rules";
--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -36,26 +36,26 @@
 $title="FusionInventory INVENTORY";
 $version="2.3.0-1";
 
-$LANG['plugin_fusinvinventory']["title"][0]="$title";
+$LANG['plugin_fusinvinventory']['title'][0]="$title";
 
-$LANG['plugin_fusinvinventory']["setup"][17]="Le plugin ".$title." a besoin que le plugin FusionInventory soit activé pour être lui-même activé.";
-$LANG['plugin_fusinvinventory']["setup"][18]="Le plugin ".$title." a besoin que le plugin FusionInventory soit activé pour être lui-même désinstallé.";
-$LANG['plugin_fusinvinventory']["setup"][20]="Options d'importation";
-$LANG['plugin_fusinvinventory']["setup"][21]="Composants";
-$LANG['plugin_fusinvinventory']["setup"][22]="Import global";
-$LANG['plugin_fusinvinventory']["setup"][23]="Pas d'import";
-$LANG['plugin_fusinvinventory']["setup"][24]="Import unique";
-$LANG['plugin_fusinvinventory']["setup"][25]="Base de registre";
-$LANG['plugin_fusinvinventory']["setup"][26]="Processus";
-$LANG['plugin_fusinvinventory']["setup"][27]="Import unique sur numéro de série";
+$LANG['plugin_fusinvinventory']['setup'][17]="Le plugin ".$title." a besoin que le plugin FusionInventory soit activé pour être lui-même activé.";
+$LANG['plugin_fusinvinventory']['setup'][18]="Le plugin ".$title." a besoin que le plugin FusionInventory soit activé pour être lui-même désinstallé.";
+$LANG['plugin_fusinvinventory']['setup'][20]="Options d'importation";
+$LANG['plugin_fusinvinventory']['setup'][21]="Composants";
+$LANG['plugin_fusinvinventory']['setup'][22]="Import global";
+$LANG['plugin_fusinvinventory']['setup'][23]="Pas d'import";
+$LANG['plugin_fusinvinventory']['setup'][24]="Import unique";
+$LANG['plugin_fusinvinventory']['setup'][25]="Base de registre";
+$LANG['plugin_fusinvinventory']['setup'][26]="Processus";
+$LANG['plugin_fusinvinventory']['setup'][27]="Import unique sur numéro de série";
 
-$LANG['plugin_fusinvinventory']["menu"][0]="Importer un fichier XML de l'agent";
-$LANG['plugin_fusinvinventory']["menu"][1]="Règles de critères";
-$LANG['plugin_fusinvinventory']["menu"][2]="BlackList";
-$LANG['plugin_fusinvinventory']["menu"][3]="Règles d'entités";
+$LANG['plugin_fusinvinventory']['menu'][0]="Importer un fichier XML de l'agent";
+$LANG['plugin_fusinvinventory']['menu'][1]="Règles de critères";
+$LANG['plugin_fusinvinventory']['menu'][2]="BlackList";
+$LANG['plugin_fusinvinventory']['menu'][3]="Règles d'entités";
 
-$LANG['plugin_fusinvinventory']["importxml"][0]="Import de fichier XML provenant de l'agent";
-$LANG['plugin_fusinvinventory']["importxml"][1]="Ordinateur importé dans GLPI";
+$LANG['plugin_fusinvinventory']['importxml'][0]="Import de fichier XML provenant de l'agent";
+$LANG['plugin_fusinvinventory']['importxml'][1]="Ordinateur importé dans GLPI";
 
 $LANG['plugin_fusinvinventory']['rule'][0]="Règles de critères d'existence d'ordinateur";
 $LANG['plugin_fusinvinventory']['rule'][1]="Critère d'existence";
@@ -73,10 +73,10 @@ $LANG['plugin_fusinvinventory']['rule'][31]="Import dans le matériel inconnu";
 $LANG['plugin_fusinvinventory']['rule'][100]="Règles d'entité";
 $LANG['plugin_fusinvinventory']['rule'][101]="Tag";
 
-$LANG['plugin_fusinvinventory']["xml"][0]="XML FusionInventory";
+$LANG['plugin_fusinvinventory']['xml'][0]="XML FusionInventory";
 
-$LANG['plugin_fusinvinventory']["blacklist"][0]="Valeurs blacklistées";
-$LANG['plugin_fusinvinventory']["blacklist"][1]="Nouvelle valeur à blacklister";
+$LANG['plugin_fusinvinventory']['blacklist'][0]="Valeurs blacklistées";
+$LANG['plugin_fusinvinventory']['blacklist'][1]="Nouvelle valeur à blacklister";
 
 $LANG['plugin_fusinvinventory']['profile'][1]="$title";
 $LANG['plugin_fusinvinventory']['profile'][2]="Règles d'existence";
