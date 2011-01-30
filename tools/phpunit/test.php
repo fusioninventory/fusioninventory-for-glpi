fcfd93e6ff8476502b90165dd82096f2013c1420
David Durieux <d.durieux@siprossii.com>
2011-01-30 14:41:24 +0100

    Update test unit to send individual xml

--- tools/phpunit/test.php
+++ tools/phpunit/test.php
@@ -17,14 +17,29 @@ function prolog() {
 }
 
 
-
 $emulatorAgent = new emulatorAgent;
-$emulatorAgent->server_urlpath = "/glpi078/plugins/fusioninventory/front/communication.php";
+$emulatorAgent->server_urlpath = "/glpi078/plugins/fusioninventory/front/plugin_fusioninventory.communication.php";
+//$emulatorAgent->server_urlpath = "/glpi072/plugins/fusioninventory/front/plugin_fusioninventory.communication.php";
+//$emulatorAgent->server_urlpath = "/plugins/fusioninventory/front/plugin_fusioninventory.communication.php";
+$emulatorAgent->server_ip="127.0.0.1";
 //$emulatorAgent->Start('192.168.1.14','62354', 'prolog');
 
-   $input_xml = file_get_contents("xml/inventory_snmp/1.2/cisco2960.xml");
+   $input_xml = '<?xml version="1.0" encoding="UTF-8"?>
+<REQUEST>
+  <DEVICEID>port003-2010-06-08-08-13-45</DEVICEID>
+  <QUERY>PROLOG</QUERY>
+  <TOKEN>CBXTMXLU</TOKEN>
+</REQUEST>';
+   $emulatorAgent->sendProlog($input_xml);
 
+
+   $input_xml = file_get_contents("xml/inventory_local/2.1.7/debian-3.1_i386.xml");
+//$time_start = microtime(true);
    $return = $emulatorAgent->sendProlog($input_xml);
+//$time_end = microtime(true);
+//$time = $time_end - $time_start;
+
+//echo "Add/update in $time seconds\n";
    print_r($return);
 
 ?>
