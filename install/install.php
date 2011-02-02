e4bad839a90f5932fb739b8fdbba25c938085706
David Durieux <d.durieux@siprossii.com>
2011-02-02 11:38:16 +0100

    set max_execution_time to 0 and disable memory limit when install plugin. references #577

--- install/install.php
+++ install/install.php
@@ -38,6 +38,8 @@
 function pluginFusinvsnmpInstall() {
    global $DB,$LANG;
 
+   ini_set("memory_limit","-1");
+   ini_set("max_execution_time", "0");
 
    // Get informations of plugin
    $a_plugin = plugin_version_fusinvsnmp();
