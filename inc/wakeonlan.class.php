b2c2a20dce99b6919d0727c36d3d93fe8b7647d9
David Durieux <d.durieux@siprossii.com>
2011-03-02 22:41:49 +0100

    Begin to fix wakeonlan tasks

--- inc/wakeonlan.class.php
+++ inc/wakeonlan.class.php
@@ -1,38 +1,36 @@
 <?php
 
 /*
- * @version $Id$
- ----------------------------------------------------------------------
- FusionInventory
- Coded by the FusionInventory Development Team.
+   ----------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2011 by the FusionInventory Development Team.
 
- http://www.fusioninventory.org/   http://forge.fusioninventory.org//
- ----------------------------------------------------------------------
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ----------------------------------------------------------------------
 
- LICENSE
+   LICENSE
 
- This file is part of FusionInventory plugins.
+   This file is part of FusionInventory.
 
- FusionInventory is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   any later version.
 
- FusionInventory is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with FusionInventory; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- ------------------------------------------------------------------------
+   You should have received a copy of the GNU General Public License
+   along with FusionInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+   Original Author of file: David DURIEUX
+   Co-authors of file:
+   Purpose of file:
+   ----------------------------------------------------------------------
  */
-
-// ----------------------------------------------------------------------
-// Original Author of file: DURIEUX David
-// Purpose of file:
-// ----------------------------------------------------------------------
 
 if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access directly to this file");
@@ -54,6 +52,85 @@ class PluginFusioninventoryWakeonlan extends PluginFusioninventoryCommunication 
       $PluginFusioninventoryTaskjob->getFromDB($taskjobs_id);
       $PluginFusioninventoryTask->getFromDB($PluginFusioninventoryTaskjob->fields['plugin_fusioninventory_tasks_id']);
 
+
+      $a_definitions = importArrayFromDB($PluginFusioninventoryTaskjob->fields['definition']);
+
+
+
+/*
+ * Case 1 : one agent defined
+ */
+
+
+/*
+ * Case 2 : many agents defined
+ */
+
+
+
+/*
+ * Case 3 : dynamic agent
+ */
+
+   // getAgentsSubnet($nb_computers, $subnet='')
+
+
+
+/*
+ * Case 4 : dynamic agent same subnet
+ */
+
+      $a_subnet = array();
+      foreach($a_definitions as $items_id) {
+         $sql = "SELECT * FROM `glpi_networkports`
+            WHERE `items_id`='".$items_id."'
+               AND `itemtype`='Computer'
+               AND `mac`!='' ";
+         if ($result = $DB->query($sql)) {
+         while ($data=$DB->fetch_array($result)) {
+            $a_subnet[$data_subnet['subnet']]++;
+         }
+      }
+   }
+   // getAgentsSubnet($nb_computers, $subnet)
+
+   }
+
+
+
+   // When agent contact server, this function send datas to agent
+   // $a_devices = array(itemtype, items_id);
+   function run($items_id, $itemtype, $taskjobs_id, $taskjobstatus_id) {
+      global $DB;
+
+      $PluginFusioninventoryTaskjobstatus = new PluginFusioninventoryTaskjobstatus;
+      $NetworkPort                        = new NetworkPort;
+
+      $sxml_option = $this->sxml->addChild('OPTION');
+      $sxml_option->addChild('NAME', 'WAKEONLAN');
+
+      // Get ip
+      $a_networkPort = $NetworkPort->find("`itemtype`='".$itemtype."' AND `items_id`='".$items_id."' ");
+      foreach ($a_networkPort as $data) {
+         if ($data['ip'] != "127.0.0.1") {
+            $sxml_param = $sxml_option->addChild('PARAM');
+            $sxml_param->addAttribute('MAC', $data['mac']);
+            $sxml_param->addAttribute('IP', $data['ip']);
+
+            // Update taskjobstatus (state = 3 : finish); Because we haven't return of agent on this action
+            $PluginFusioninventoryTaskjobstatus->changeStatusFinish($taskjobs_id, $items_id, $itemtype, 0, 'WakeOnLan have not return state', 1);
+         }
+      }
+      return $this->sxml;
+   }
+
+
+   
+   function getAgentsSubnet($nb_computers, $subnet='') {
+
+      $PluginFusioninventoryAgentmodule = new PluginFusioninventoryAgentmodule();
+      $OperatingSystem = new OperatingSystem();
+
       // Get ids of operating systems which can make real wakeonlan
       $a_os = $OperatingSystem->find(" `name` LIKE '%Linux%' ");
       $osfind = '(';
@@ -74,7 +151,13 @@ class PluginFusioninventoryWakeonlan extends PluginFusioninventoryCommunication 
          $pass_count++;
          $osfind = 'AND operatingsystems_id IN '.$osfind;
       }
+
       for ($pass = 0; $pass < $pass_count; $pass++) {
+
+         
+
+
+
          if ($pass == "1") {
             // It's not linux
             $osfind = str_replace('AND operatingsystems_id IN ', 'AND operatingsystems_id NOT IN ', $osfind);
@@ -83,43 +166,14 @@ class PluginFusioninventoryWakeonlan extends PluginFusioninventoryCommunication 
          // Get subnet of device
          $query_subnet = "SELECT * FROM `glpi_networkports`
             WHERE `items_id`='".$items_id."'
-               AND `itemtype`='".$itemtype."'
+               AND `itemtype`='Computer'
                AND `mac`!='' ";
          if ($result_subnet = $DB->query($query_subnet)) {
             while ($data_subnet=$DB->fetch_array($result_subnet)) {
-               $agentModule = $PluginFusioninventoryAgentmodule->getActivationExceptions('WAKEONLAN');
-               $where = "";
-               if ($agentModule['is_active'] == 0) {
-                  $a_agentList = importArrayFromDB($agentModule['exceptions']);
-                  if (count($a_agentList) > 0) {
-                     $where = " AND `glpi_plugin_fusioninventory_agents`.`ID` IN (";
-                     $i = 0;
-                     $sep  = '';
-                     foreach ($a_agentList as $agent) {
-                        if ($i> 1) {
-                           $sep  = ',';
-                        }
-                        $where .= $agent['id'].$sep;
-                        $i++;
-                     }
-                     $where .= ") ";
-                  }
-               } else {
-                  $a_agentList = importArrayFromDB($agentModule['exceptions']);
-                  if (count($a_agentList) > 0) {
-                     $where = " AND `glpi_plugin_fusioninventory_agents`.`ID` NOT IN (";
-                     $i = 0;
-                     $sep  = '';
-                     foreach ($a_agentList as $agent) {
-                        if ($i> 1) {
-                           $sep  = ',';
-                        }
-                        $where .= $agent['id'].$sep;
-                        $i++;
-                     }
-                     $where .= ") ";
-                  }
-               }
+               $a_agents = $PluginFusioninventoryAgentmodule->getAgentsCanDo('WAKEONLAN');
+               $where = " AND `glpi_plugin_fusioninventory_agents`.`ID` IN (";
+               $where .= implode(',', $a_agents);
+               $where .= ") ";
 
                $query = "SELECT `glpi_plugin_fusioninventory_agents`.`id` as `a_id`, ip, subnet, token FROM `glpi_plugin_fusioninventory_agents`
                   LEFT JOIN `glpi_networkports` ON `glpi_networkports`.`items_id` = `glpi_plugin_fusioninventory_agents`.`items_id`
@@ -151,34 +205,6 @@ class PluginFusioninventoryWakeonlan extends PluginFusioninventoryCommunication 
             }
          }
       }
-   }
-
-
-
-   // When agent contact server, this function send datas to agent
-   // $a_devices = array(itemtype, items_id);
-   function run($items_id, $itemtype, $taskjobs_id, $taskjobstatus_id) {
-      global $DB;
-
-      $PluginFusioninventoryTaskjobstatus = new PluginFusioninventoryTaskjobstatus;
-      $NetworkPort                        = new NetworkPort;
-
-      $sxml_option = $this->sxml->addChild('OPTION');
-      $sxml_option->addChild('NAME', 'WAKEONLAN');
-
-      // Get ip
-      $a_networkPort = $NetworkPort->find("`itemtype`='".$itemtype."' AND `items_id`='".$items_id."' ");
-      foreach ($a_networkPort as $data) {
-         if ($data['ip'] != "127.0.0.1") {
-            $sxml_param = $sxml_option->addChild('PARAM');
-            $sxml_param->addAttribute('MAC', $data['mac']);
-            $sxml_param->addAttribute('IP', $data['ip']);
-
-            // Update taskjobstatus (state = 3 : finish); Because we haven't return of agent on this action
-            $PluginFusioninventoryTaskjobstatus->changeStatusFinish($taskjobs_id, $items_id, $itemtype, 0, 'WakeOnLan have not return state', 1);
-         }
-      }
-      return $this->sxml;
    }
 }
 
