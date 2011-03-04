e74a8fa6c2d2272872409075f10298fb0985511c
David Durieux <d.durieux@siprossii.com>
2011-03-04 14:17:10 +0100

    Update LANG to 2.4.0 and create SQL file for 2.4.0

--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -33,7 +33,7 @@
  */
 
 $title="FusionInventory SNMP";
-$version="2.3.0-1";
+$version="2.4.0-1";
 
 $LANG['plugin_fusinvsnmp']['title'][0]="$title";
 $LANG['plugin_fusinvsnmp']['title'][1]="Informations SNMP";
