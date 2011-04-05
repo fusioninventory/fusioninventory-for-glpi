1d1eeef2de9e8c8d66febf4e4ef533b5f28c9bd3
David Durieux <d.durieux@siprossii.com>
2011-04-05 13:33:23 +0200

    Fix lang DE

--- locales/de_DE.php
+++ locales/de_DE.php
@@ -47,6 +47,8 @@ $LANG['plugin_fusinvinventory']['setup'][24]="Einmaliger Import";
 $LANG['plugin_fusinvinventory']['setup'][25]="Registrierungsdatei";
 $LANG['plugin_fusinvinventory']['setup'][26]="Prozess";
 $LANG['plugin_fusinvinventory']['setup'][27]="Einmaliger Import via Seriennummer";
+$LANG['plugin_fusinvinventory']['setup'][28]="Automatic computers transfer";
+$LANG['plugin_fusinvinventory']['setup'][29]="Model for automatic computers transfer in an other entity";
 
 $LANG['plugin_fusinvinventory']['menu'][0]="XML-Datei eines Agents importieren";
 $LANG['plugin_fusinvinventory']['menu'][1]="Kriterien";
