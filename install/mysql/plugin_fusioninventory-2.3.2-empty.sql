acbae508a71b18bfa0846da7f098b4c4b2c81aa9
David Durieux <d.durieux@siprossii.com>
2011-05-02 10:57:46 +0200

    Fix update version

---
+++ install/mysql/plugin_fusioninventory-2.3.2-empty.sql
@@ -0,0 +1,283 @@
+## obsolete tables
+DROP TABLE IF EXISTS `glpi_dropdown_plugin_fusioninventory_snmp_auth_auth_protocol`;
+DROP TABLE IF EXISTS `glpi_dropdown_plugin_fusioninventory_snmp_auth_priv_protocol`;
+DROP TABLE IF EXISTS `glpi_dropdown_plugin_fusioninventory_snmp_version`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_agents_errors`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_agents_inventory_state`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_agentprocesses`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_computers`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_config_modules`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_config_snmp_networking`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_connection_history`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_connection_stats`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_discovery`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_errors`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_unknown_mac`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_walks`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_lockable`;
+
+## renamed tables
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_config`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_config_modules`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_lock`;
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_task`;
+#DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_unknown_device`;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_agents`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_agents` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `entities_id` int(11) NOT NULL DEFAULT '0',
+   `is_recursive` tinyint(1) NOT NULL DEFAULT '1',
+   `name` varchar(255) DEFAULT NULL,
+   `last_contact` datetime DEFAULT NULL,
+   `version` varchar(255) DEFAULT NULL,
+   `lock` int(1) NOT NULL DEFAULT '0',
+   `device_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'XML <DEVICE_ID> TAG VALUE',
+   `items_id` int(11) NOT NULL DEFAULT '0',
+   `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `token` varchar(255) COLLATE utf8_unicode_ci NULL,
+   PRIMARY KEY (`id`),
+   KEY `name` (`name`),
+   KEY `device_id` (`device_id`),
+   KEY `item` (`itemtype`,`items_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_configs`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_configs` (
+   `id` int(1) NOT NULL AUTO_INCREMENT,
+   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+   `value` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `plugins_id` int(11) NOT NULL DEFAULT '0',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `unicity` (`type`, `plugins_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_locks`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_locks` (
+   `id` INT( 11 ) NOT NULL AUTO_INCREMENT,
+   `tablename` VARCHAR( 64 ) COLLATE utf8_unicode_ci NOT NULL,
+   `items_id` INT( 11 ) NOT NULL,
+   `tablefields` TEXT,
+   PRIMARY KEY ( `id` ),
+   KEY `tablename` ( `tablename` ),
+   KEY `items_id` (`items_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_tasks`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `entities_id` int(11) NOT NULL DEFAULT '-1',
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `comment` text COLLATE utf8_unicode_ci,
+  `is_active` int(1) NOT NULL DEFAULT '0',
+  `communication` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'push',
+  `permanent` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_scheduled` datetime DEFAULT NULL,
+  `periodicity_count` int(6) NOT NULL DEFAULT '0',
+  `periodicity_type` varchar(255) NULL,
+  `execution_id` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `entities_id` ( `entities_id` ),
+  KEY `is_active` ( `is_active` )
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_taskjobs`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_taskjobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plugin_fusioninventory_tasks_id` int(11) NOT NULL DEFAULT '0',
+  `entities_id` int(11) NOT NULL DEFAULT '-1',
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `date_creation` datetime DEFAULT NULL,
+  `retry_nb` int(2) NOT NULL DEFAULT '0',
+  `retry_time` int(11) NOT NULL DEFAULT '0',
+  `plugins_id` int(11) NOT NULL DEFAULT '0',
+  `method` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `definition` text COLLATE utf8_unicode_ci,
+  `action` text COLLATE utf8_unicode_ci,
+  `comment` text COLLATE utf8_unicode_ci,
+  `users_id` int(11) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `rescheduled_taskjob_id` int(11) NOT NULL DEFAULT '0',
+  `statuscomments` text COLLATE utf8_unicode_ci,
+  `periodicity_count` int(6) NOT NULL DEFAULT '0',
+  `periodicity_type` varchar(255) NULL,
+  `execution_id` int(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `plugin_fusioninventory_tasks_id` (`plugin_fusioninventory_tasks_id`),
+  KEY `entities_id` (`entities_id`),
+  KEY `plugins_id` (`plugins_id`),
+  KEY `users_id` (`users_id`),
+  KEY `rescheduled_taskjob_id` (`rescheduled_taskjob_id`),
+  KEY `method` (`method`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_taskjoblogs`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_taskjoblogs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `plugin_fusioninventory_taskjobstatus_id` int(11) NOT NULL DEFAULT '0',
+  `date` datetime DEFAULT NULL,
+  `items_id` int(11) NOT NULL DEFAULT '0',
+  `itemtype` varchar(100) DEFAULT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `comment` text,
+  PRIMARY KEY (`id`),
+  KEY `plugin_fusioninventory_taskjobstatus_id` (`plugin_fusioninventory_taskjobstatus_id`,`state`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_taskjobstatus`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_taskjobstatus` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `plugin_fusioninventory_taskjobs_id` int(11) NOT NULL DEFAULT '0',
+  `items_id` int(11) NOT NULL DEFAULT '0',
+  `itemtype` varchar(100) DEFAULT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
+  `plugin_fusioninventory_agents_id` int(11) NOT NULL DEFAULT '0',
+  `specificity` varchar(255) DEFAULT NULL,
+  `uniqid` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `plugin_fusioninventory_taskjobs_id` (`plugin_fusioninventory_taskjobs_id`),
+  KEY `plugin_fusioninventory_agents_id` (`plugin_fusioninventory_agents_id`,`state`),
+  KEY `plugin_fusioninventory_taskjob_2` (`plugin_fusioninventory_taskjobs_id`,`state`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_profiles`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_profiles` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+   `right` char(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `plugins_id` int(11) NOT NULL DEFAULT '0',
+   `profiles_id` int(11) NOT NULL DEFAULT '0',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `unicity` (`type`, `plugins_id`, `profiles_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_mappings`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_mappings` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `itemtype` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `table` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `tablefield` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `locale` INT( 4 ) NOT NULL,
+   `shortlocale` INT( 4 ) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `name` (`name`),
+   KEY `itemtype` (`itemtype`),
+   KEY `table` (`table`),
+   KEY `tablefield` (`tablefield`)
+--   UNIQUE KEY `unicity` (`name`, `itemtype`) -- Specified key was too long; max key length is 1000 bytes
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_unknowndevices`;
+
+CREATE TABLE IF NOT EXISTS `glpi_plugin_fusioninventory_unknowndevices` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `date_mod` datetime DEFAULT NULL,
+   `entities_id` int(11) NOT NULL DEFAULT '0',
+   `locations_id` int(11) NOT NULL DEFAULT '0',
+   `is_deleted` smallint(6) NOT NULL DEFAULT '0',
+   `serial` VARCHAR( 255 ) NULL DEFAULT NULL,
+   `otherserial` VARCHAR( 255 ) NULL DEFAULT NULL,
+   `contact` VARCHAR( 255 ) NULL DEFAULT NULL,
+   `domain` INT( 11 ) NOT NULL DEFAULT '0',
+   `comment` TEXT NULL DEFAULT NULL,
+   `item_type` VARCHAR( 255 ) NULL DEFAULT NULL,
+   `accepted` INT( 1 ) NOT NULL DEFAULT '0',
+   `plugin_fusioninventory_agents_id` int(11) NOT NULL DEFAULT '0',
+   `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `mac` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `hub` int(1) NOT NULL DEFAULT '0',
+   `states_id` int(11) NOT NULL DEFAULT '0',
+   PRIMARY KEY (`id`),
+   KEY `entities_id` (`entities_id`),
+   KEY `plugin_fusioninventory_agents_id` (`plugin_fusioninventory_agents_id`),
+   KEY `is_deleted` (`is_deleted`),
+   KEY `date_mod` (`date_mod`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `glpi_plugin_fusioninventory_agentmodules`;
+
+CREATE TABLE `glpi_plugin_fusioninventory_agentmodules` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `plugins_id` int(11) NOT NULL DEFAULT '0',
+   `modulename` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+   `is_active` int(1) NOT NULL DEFAULT '0',
+   `exceptions` TEXT COMMENT 'array(agent_id)',
+   `entities_id` int(11) NOT NULL DEFAULT '-1',
+   PRIMARY KEY (`id`),
+   UNIQUE KEY `unicity` (`plugins_id`, `modulename`),
+   KEY `is_active` (`is_active`),
+   KEY `entities_id` (`entities_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+## INSERT
+## glpi_displaypreferences
+INSERT INTO `glpi_displaypreferences` (`id`, `itemtype`, `num`, `rank`, `users_id`) 
+   VALUES (NULL,'PluginFusioninventoryAgent', '2', '1', '0'),
+          (NULL,'PluginFusioninventoryAgent', '4', '2', '0'),
+          (NULL,'PluginFusioninventoryAgent', '5', '3', '0'),
+          (NULL,'PluginFusioninventoryAgent', '6', '4', '0'),
+          (NULL,'PluginFusioninventoryAgent', '7', '5', '0'),
+          (NULL,'PluginFusioninventoryAgent', '8', '6', '0'),
+          (NULL,'PluginFusioninventoryAgent', '9', '7', '0'),
+
+          (NULL, 'PluginFusioninventoryUnknownDevice', '2', '1', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '4', '2', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '3', '3', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '5', '4', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '7', '5', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '10', '6', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '11', '7', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '18', '8', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '14', '9', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '15', '10', '0'),
+          (NULL, 'PluginFusioninventoryUnknownDevice', '9', '11', '0'),
+
+          (NULL, 'PluginFusioninventoryTask', '2', '1', '0'),
+          (NULL, 'PluginFusioninventoryTask', '3', '2', '0'),
+          (NULL, 'PluginFusioninventoryTask', '4', '3', '0'),
+          (NULL, 'PluginFusioninventoryTask', '5', '4', '0'),
+          (NULL, 'PluginFusioninventoryTask', '6', '5', '0'),
+          (NULL, 'PluginFusioninventoryTask', '7', '6', '0'),
+          (NULL, 'PluginFusioninventoryTask', '30', '7', '0'),
+
+          (NULL,'PluginFusioninventoryTaskjob', '1', '1', '0'),
+          (NULL,'PluginFusioninventoryTaskjob', '2', '2', '0'),
+          (NULL,'PluginFusioninventoryTaskjob', '3', '3', '0'),
+          (NULL,'PluginFusioninventoryTaskjob', '4', '4', '0'),
+          (NULL,'PluginFusioninventoryTaskjob', '5', '5', '0');
