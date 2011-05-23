4c703bf5f7886552ecee2eefbb9a99a0369e8573
David Durieux <d.durieux@siprossii.com>
2011-05-23 14:45:49 +0200

    Update 2.3.4

--- install/update.php
+++ install/update.php
@@ -65,6 +65,7 @@ function pluginFusinvinventoryUpdate($current_version) {
          include("update_231_232.php");
          update231to232();
       case "2.3.2-1":
+      case "2.3.3-1":
 
    }
 
