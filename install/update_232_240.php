967965212bebb0887ca15d5e5d39c9ae52ca3bae
David Durieux <d.durieux@siprossii.com>
2011-07-18 21:58:40 +0200

    Fix when reinstall only fusinvsnmp plugin, mapping add a time more each mapping name. closes #1016

--- install/update_232_240.php
+++ install/update_232_240.php
@@ -128,5 +128,44 @@ function update232to240() {
           ('NetworkEquipment','cdpCachePlatform','','',437,NULL)";
    $DB->query($query);
    
+   // Fix problem with mapping with many entries with same mapping
+   $a_mapping = array();
+   $a_mappingdouble = array();
+   $query = "SELECT * FROM `glpi_plugin_fusioninventory_mappings`
+      ORDER BY `id`";
+   $result=$DB->query($query);
+   while ($data=$DB->fetch_array($result)) {
+      if (!isset($a_mapping[$data['itemtype'].".".$data['name']])) {
+         $a_mapping[$data['itemtype'].".".$data['name']] = $data['id'];
+      } else {
+         $a_mappingdouble[$data['id']] = $data['itemtype'].".".$data['name'];
+      }
+   }   
+   foreach($a_mappingdouble as $mapping_id=>$mappingkey) {
+      $query = "UPDATE `glpi_plugin_fusinvsnmp_modelmibs`
+         SET plugin_fusioninventory_mappings_id='".$a_mapping[$mappingkey]."'
+         WHERE plugin_fusioninventory_mappings_id='".$mapping_id."'";
+      $DB->query($query);
+      $query = "UPDATE `glpi_plugin_fusinvsnmp_printercartridges`
+         SET plugin_fusioninventory_mappings_id='".$a_mapping[$mappingkey]."'
+         WHERE plugin_fusioninventory_mappings_id='".$mapping_id."'";
+      $DB->query($query);
+      $query = "UPDATE `glpi_plugin_fusinvsnmp_networkportlogs`
+         SET plugin_fusioninventory_mappings_id='".$a_mapping[$mappingkey]."'
+         WHERE plugin_fusioninventory_mappings_id='".$mapping_id."'";
+      $DB->query($query);
+      $query = "UPDATE `glpi_plugin_fusinvsnmp_configlogfields`
+         SET plugin_fusioninventory_mappings_id='".$a_mapping[$mappingkey]."'
+         WHERE plugin_fusioninventory_mappings_id='".$mapping_id."'";
+      $DB->query($query);
+      $query = "DELETE FROM `glpi_plugin_fusioninventory_mappings`
+         WHERE `id` = '".$mapping_id."'";
+      $DB->query($query);
+   }
+
+   
+   
+   
+   
 }
 ?>
