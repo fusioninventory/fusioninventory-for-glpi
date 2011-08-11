b03e2e323a8e78ab355e094114e55a3989411a0b
David Durieux <d.durieux@siprossii.com>
2011-08-11 22:41:08 +0200

    Fix functions into Toolbox class

--- install/install.php
+++ install/install.php
@@ -61,7 +61,7 @@ function pluginFusinvinventoryInstall() {
          $sql_query = fread($DBf_handle, filesize($DB_file));
          fclose($DBf_handle);
          foreach ( explode(";\n", "$sql_query") as $sql_line) {
-            if (get_magic_quotes_runtime()) $sql_line=stripslashes_deep($sql_line);
+            if (get_magic_quotes_runtime()) $sql_line=Toolbox::stripslashes_deep($sql_line);
             if (!empty($sql_line)) {
                $DB->query($sql_line)/* or die($DB->error())*/;
             }
