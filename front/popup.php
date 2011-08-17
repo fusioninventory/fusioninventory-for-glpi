cbb40c6ffc8d9209db8a5bb1d14a4db251a0742b
David Durieux <d.durieux@siprossii.com>
2011-08-17 18:29:44 +0200

    Fix functions for Html class

--- front/popup.php
+++ front/popup.php
@@ -44,17 +44,17 @@ if (isset($_GET["popup"])) {
 if (isset($_SESSION["glpipopup"]["name"])) {
    switch ($_SESSION["glpipopup"]["name"]) {
       case "test_rule" :
-         popHeader($LANG['buttons'][50],$_SERVER['PHP_SELF']);
+         Html::popHeader($LANG['buttons'][50],$_SERVER['PHP_SELF']);
          include "../../../front/rule.test.php";
          break;
 
       case "test_all_rules" :
-         popHeader($LANG['rulesengine'][84],$_SERVER['PHP_SELF']);
+         Html::popHeader($LANG['rulesengine'][84],$_SERVER['PHP_SELF']);
          include "../../../front/rulesengine.test.php";
          break;
 
       case "show_cache" :
-         popHeader($LANG['rulesengine'][100],$_SERVER['PHP_SELF']);
+         Html::popHeader($LANG['rulesengine'][100],$_SERVER['PHP_SELF']);
          include "../../../front/rule.cache.php";
          break;
    }
