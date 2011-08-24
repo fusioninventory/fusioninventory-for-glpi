c3e36168f879bce1fd3eef1758fff625ade11f54
David Durieux <d.durieux@siprossii.com>
2011-08-24 16:42:00 +0200

    Fix functions for Session class

--- inc/taskjob.class.php
+++ inc/taskjob.class.php
@@ -1066,7 +1066,7 @@ class PluginFusioninventoryTaskjob extends CommonDBTM {
    *
    **/
    function reenableusemode() {
-      if ($_SESSION['glpi_use_mode'] == DEBUG_MODE){
+      if ($_SESSION['glpi_use_mode'] == Session::DEBUG_MODE){
          ini_set('display_errors', 'On');
          error_reporting(E_ALL | E_STRICT);
          set_error_handler("userErrorHandler");
