be6225c1a3fbe6da54ffbcee7e5ffeb7229fe2be
David Durieux <d.durieux@siprossii.com>
2011-08-25 08:57:34 +0200

    Fix navigation in statediscovery. references #997

--- inc/statediscovery.class.php
+++ inc/statediscovery.class.php
@@ -122,7 +122,7 @@ class PluginFusinvsnmpStateDiscovery extends CommonDBTM {
       $number = $DB->numrows($resultcount);
 
       // Display the pager
-      printPager($start,$number,$CFG_GLPI['root_doc']."/plugins/fusinvsnmp/front/stateinventory.php",'');
+      printPager($start,$number,$CFG_GLPI['root_doc']."/plugins/fusinvsnmp/front/statediscovery.php",'');
 
       echo "<table class='tab_cadre_fixe'>";
 
