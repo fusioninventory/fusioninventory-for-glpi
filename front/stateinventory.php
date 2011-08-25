bea9366a30419cd5d71fe16e9e91ad181f70f156
David Durieux <d.durieux@siprossii.com>
2011-08-25 08:54:57 +0200

    Fix rights for stateinventory. references #997

--- front/stateinventory.php
+++ front/stateinventory.php
@@ -38,7 +38,7 @@ include (GLPI_ROOT . "/inc/includes.php");
 
 commonHeader($LANG['plugin_fusioninventory']['title'][0],$_SERVER["PHP_SELF"],"plugins","fusioninventory","stateinventory");
 
-//PluginFusioninventoryProfile::checkRight("fusinvsnmp", "statediscovery","r");
+PluginFusioninventoryProfile::checkRight("fusioninventory", "task","r");
 
 PluginFusioninventoryMenu::displayMenu("mini");
 
