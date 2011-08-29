a1bd52ea6b6b4a28d148c141a922f5bc06485add
David Durieux <d.durieux@siprossii.com>
2011-08-29 13:27:56 +0200

    Update lang

---
+++ locales/es_ES.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+   ----------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2011 by the FusionInventory Development Team.
+
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ----------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of FusionInventory.
+
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   any later version.
+
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FusionInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+   Original Author of file: David DURIEUX
+   Co-authors of file: 
+   Purpose of file:
+   Not translate this file, use https://www.transifex.net/projects/p/FusionInventory/
+   ----------------------------------------------------------------------
+ */
+
+$LANG['plugin_fusinvinventory']['antivirus'][0]="Antivirus";
+$LANG['plugin_fusinvinventory']['antivirus'][1]="Sin antivirus en este ordenador";
+$LANG['plugin_fusinvinventory']['antivirus'][2]="Versión";
+$LANG['plugin_fusinvinventory']['antivirus'][3]="Actualizado";
+
+$LANG['plugin_fusinvinventory']['bios'][0]="Bios";
+
+$LANG['plugin_fusinvinventory']['blacklist'][0]="valor en lista negra";
+$LANG['plugin_fusinvinventory']['blacklist'][1]="Nuevo valor a la lista negra";
+
+$LANG['plugin_fusinvinventory']['computer'][0]="Último inventario";
+
+$LANG['plugin_fusinvinventory']['importxml'][0]="Importar fichero XML desde un agente";
+$LANG['plugin_fusinvinventory']['importxml'][1]="Ordenador importado en GLPI";
+$LANG['plugin_fusinvinventory']['importxml'][2]="¡No hay fichero pra importar!";
+$LANG['plugin_fusinvinventory']['importxml'][3]="¡Fichero XML no válido!";
+
+$LANG['plugin_fusinvinventory']['integrity'][0]="Only in GLPI (check to delete)";
+$LANG['plugin_fusinvinventory']['integrity'][1]="Only in last inventory (check to import)";
+
+$LANG['plugin_fusinvinventory']['menu'][0]="Fichero XML del agente de importación";
+$LANG['plugin_fusinvinventory']['menu'][1]="Reglas de selección";
+$LANG['plugin_fusinvinventory']['menu'][2]="Lista negra";
+$LANG['plugin_fusinvinventory']['menu'][4]="Comprobar integridad de datos";
+
+$LANG['plugin_fusinvinventory']['profile'][2]="Criterios de existencia";
+$LANG['plugin_fusinvinventory']['profile'][3]="Importación manual del fichero XML";
+$LANG['plugin_fusinvinventory']['profile'][4]="Lista negra de campos";
+
+$LANG['plugin_fusinvinventory']['rule'][0]="Reglas para ordenadores existentes";
+$LANG['plugin_fusinvinventory']['rule'][100]="Reglas de entidad";
+$LANG['plugin_fusinvinventory']['rule'][102]="Ignorar en importación de FusionInventory";
+$LANG['plugin_fusinvinventory']['rule'][1]="Criterio de existencia";
+$LANG['plugin_fusinvinventory']['rule'][2]="Nº de serie";
+$LANG['plugin_fusinvinventory']['rule'][30]="Importar en asset";
+$LANG['plugin_fusinvinventory']['rule'][31]="Importar en dispositivos desconocidos";
+$LANG['plugin_fusinvinventory']['rule'][3]="Dirección MAC";
+$LANG['plugin_fusinvinventory']['rule'][4]="Clave de producto Microsoft";
+$LANG['plugin_fusinvinventory']['rule'][5]="Modelo de ordenador";
+$LANG['plugin_fusinvinventory']['rule'][6]="Nº serie disco duro";
+$LANG['plugin_fusinvinventory']['rule'][7]="Nº serie de particiones";
+$LANG['plugin_fusinvinventory']['rule'][8]="Etiqueta (Tag)";
+
+$LANG['plugin_fusinvinventory']['setup'][17]="El plugin FusionInventory INVENTORY necesita el plugin FusionInventory activado antes de su activación.";
+$LANG['plugin_fusinvinventory']['setup'][18]="Plugin FusionInventory INVENTORY need plugin FusionInventory activated before uninstall.";
+$LANG['plugin_fusinvinventory']['setup'][20]="Opciones de importación";
+$LANG['plugin_fusinvinventory']['setup'][21]="Componentes";
+$LANG['plugin_fusinvinventory']['setup'][22]="Importación global";
+$LANG['plugin_fusinvinventory']['setup'][23]="No importar";
+$LANG['plugin_fusinvinventory']['setup'][24]="Importacion única";
+$LANG['plugin_fusinvinventory']['setup'][25]="Registrar";
+$LANG['plugin_fusinvinventory']['setup'][26]="Proceso";
+$LANG['plugin_fusinvinventory']['setup'][27]="Importacion única por nº de serie";
+$LANG['plugin_fusinvinventory']['setup'][28]="Transferencia automática de ordenadores";
+$LANG['plugin_fusinvinventory']['setup'][29]="Modelo para la transferencia automática de ordenadores a otra entidad";
+$LANG['plugin_fusinvinventory']['setup'][30]="Network drives";
+$LANG['plugin_fusinvinventory']['setup'][31]="Virtual network card";
+$LANG['plugin_fusinvinventory']['setup'][32]="This option will not import this item";
+$LANG['plugin_fusinvinventory']['setup'][33]="This option will merge items with same name to reduce number of items if this management isn't important";
+$LANG['plugin_fusinvinventory']['setup'][34]="This option will create one item for each item found";
+$LANG['plugin_fusinvinventory']['setup'][35]="This option will create one item for each item have serial number";
+$LANG['plugin_fusinvinventory']['setup'][36]="Default status";
+
+$LANG['plugin_fusinvinventory']['title'][0]="FusionInventory INVENTORY";
+$LANG['plugin_fusinvinventory']['title'][1]="Local inventory";
+$LANG['plugin_fusinvinventory']['title'][2]="Vmware host remote inventory";
+
+$LANG['plugin_fusinvinventory']['vmwareesx'][0]="Vmware host";
+?>
---
+++ locales/et_EE.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+   ----------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2011 by the FusionInventory Development Team.
+
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ----------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of FusionInventory.
+
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   any later version.
+
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FusionInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+   Original Author of file: David DURIEUX
+   Co-authors of file: 
+   Purpose of file:
+   Not translate this file, use https://www.transifex.net/projects/p/FusionInventory/
+   ----------------------------------------------------------------------
+ */
+
+$LANG['plugin_fusinvinventory']['antivirus'][0]="Viirusetõrje";
+$LANG['plugin_fusinvinventory']['antivirus'][1]="No antivirus on this computer";
+$LANG['plugin_fusinvinventory']['antivirus'][2]="Versioon";
+$LANG['plugin_fusinvinventory']['antivirus'][3]="Ajakohane";
+
+$LANG['plugin_fusinvinventory']['bios'][0]="Bios";
+
+$LANG['plugin_fusinvinventory']['blacklist'][0]="blacklisted value";
+$LANG['plugin_fusinvinventory']['blacklist'][1]="New value to blacklist";
+
+$LANG['plugin_fusinvinventory']['computer'][0]="Viimane laoseis";
+
+$LANG['plugin_fusinvinventory']['importxml'][0]="Import XML file from an Agent";
+$LANG['plugin_fusinvinventory']['importxml'][1]="Computer injected into GLPI";
+$LANG['plugin_fusinvinventory']['importxml'][2]="No file to import!";
+$LANG['plugin_fusinvinventory']['importxml'][3]="XML-fail pole korrektne!";
+
+$LANG['plugin_fusinvinventory']['integrity'][0]="Only in GLPI (check to delete)";
+$LANG['plugin_fusinvinventory']['integrity'][1]="Only in last inventory (check to import)";
+
+$LANG['plugin_fusinvinventory']['menu'][0]="Import agent XML file";
+$LANG['plugin_fusinvinventory']['menu'][1]="Kriteeriumi reeglid";
+$LANG['plugin_fusinvinventory']['menu'][2]="Must nimekiri";
+$LANG['plugin_fusinvinventory']['menu'][4]="Andmete korrasoleku kontroll";
+
+$LANG['plugin_fusinvinventory']['profile'][2]="Existance criteria";
+$LANG['plugin_fusinvinventory']['profile'][3]="XML file manual import";
+$LANG['plugin_fusinvinventory']['profile'][4]="Fields blacklist";
+
+$LANG['plugin_fusinvinventory']['rule'][0]="Computer existent criteria rules";
+$LANG['plugin_fusinvinventory']['rule'][100]="Entity rules";
+$LANG['plugin_fusinvinventory']['rule'][102]="Ignore in FusionInventory import";
+$LANG['plugin_fusinvinventory']['rule'][1]="Olemasolev kriteerium";
+$LANG['plugin_fusinvinventory']['rule'][2]="Seerianumber";
+$LANG['plugin_fusinvinventory']['rule'][30]="Import in asset";
+$LANG['plugin_fusinvinventory']['rule'][31]="Import in unknown devices";
+$LANG['plugin_fusinvinventory']['rule'][3]="MAC aadress";
+$LANG['plugin_fusinvinventory']['rule'][4]="Microsofti tootevõti";
+$LANG['plugin_fusinvinventory']['rule'][5]="Arvuti mudel";
+$LANG['plugin_fusinvinventory']['rule'][6]="Kõvaketta seerianumber";
+$LANG['plugin_fusinvinventory']['rule'][7]="Partitions serial number";
+$LANG['plugin_fusinvinventory']['rule'][8]="Silt";
+
+$LANG['plugin_fusinvinventory']['setup'][17]="Plugin FusionInventory INVENTORY need plugin FusionInventory activated before activation.";
+$LANG['plugin_fusinvinventory']['setup'][18]="Plugin FusionInventory INVENTORY need plugin FusionInventory activated before uninstall.";
+$LANG['plugin_fusinvinventory']['setup'][20]="Importimise valikud";
+$LANG['plugin_fusinvinventory']['setup'][21]="Komponendid";
+$LANG['plugin_fusinvinventory']['setup'][22]="Üldine import";
+$LANG['plugin_fusinvinventory']['setup'][23]="Importi pole";
+$LANG['plugin_fusinvinventory']['setup'][24]="Ainulaadne import";
+$LANG['plugin_fusinvinventory']['setup'][25]="Register";
+$LANG['plugin_fusinvinventory']['setup'][26]="Processus";
+$LANG['plugin_fusinvinventory']['setup'][27]="Unique import on serial number";
+$LANG['plugin_fusinvinventory']['setup'][28]="Automatic computers transfer";
+$LANG['plugin_fusinvinventory']['setup'][29]="Model for automatic computers transfer in an other entity";
+$LANG['plugin_fusinvinventory']['setup'][30]="Network drives";
+$LANG['plugin_fusinvinventory']['setup'][31]="Virtual network card";
+$LANG['plugin_fusinvinventory']['setup'][32]="This option will not import this item";
+$LANG['plugin_fusinvinventory']['setup'][33]="This option will merge items with same name to reduce number of items if this management isn't important";
+$LANG['plugin_fusinvinventory']['setup'][34]="This option will create one item for each item found";
+$LANG['plugin_fusinvinventory']['setup'][35]="This option will create one item for each item have serial number";
+$LANG['plugin_fusinvinventory']['setup'][36]="Vaikimisi staatus";
+
+$LANG['plugin_fusinvinventory']['title'][0]="FusionInventory INVENTORY";
+$LANG['plugin_fusinvinventory']['title'][1]="Kohalik laoseis";
+$LANG['plugin_fusinvinventory']['title'][2]="Vmware host remote inventory";
+
+$LANG['plugin_fusinvinventory']['vmwareesx'][0]="Vmware host";
+?>
