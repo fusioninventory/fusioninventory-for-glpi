07f0ec615d6ae9f8238d01b39ae47c388842ca99
David Durieux <d.durieux@siprossii.com>
2011-08-29 11:07:00 +0200

    Update tools for transifex (translation)

--- tools/create_glpilangfilefrompo.pl
+++ tools/create_glpilangfilefrompo.pl
@@ -1,23 +1,83 @@
 use Locale::PO;
 use Data::Dumper;
 use IO::File;
+use Path::Class;
 
-if (not defined $ARGV[1]) {
+if (not defined $ARGV[2]) {
    print "Error !
-Use : perl create_glpilangfilefrompo.pl pluginname po.file
+Use : perl create_glpilangfilefrompo.pl pluginname po.file 0/1
+O/1 => complete file with english(1) or keep empty (0) for word not translated
 exiting...\n";
+exit;
 }
 
-my $aref = Locale::PO->load_file_ashash($ARGV[1]);
+my $user = 'ddurieux';
+my $password = '';
+my $transifexhost = 'http://www.transifex.net/';
+my $remoteurltranslations = 'http://www.transifex.net/projects/p/FusionInventory/resource/Plugin_FusionInventory/';
 
-my $file = $ARGV[1].'.php';
+`tx init --user=$user --pass=$password --host=$transifexhost`;
+`tx set --auto-remote $remoteurltranslations`;
+`tx pull -a`;
+
+my $dir  = dir('translations/FusionInventory.Plugin_FusionInventory');
+my @filesdir = $dir->children;
+
+foreach my $filename (@filesdir) {
+
+
+my $aref = Locale::PO->load_file_ashash($filename);
+
+my $file = $filename.'.php';
+$file =~ s/.po//;
+$file =~ s/translations\/FusionInventory.Plugin_FusionInventory\///;
 my $fh = IO::File->new($file,'>')
    or die "can't open file";
 $fh->print("<?php\n");
+$fh->print("
+/*
+   ----------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2011 by the FusionInventory Development Team.
+
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ----------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of FusionInventory.
+
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   any later version.
+
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FusionInventory.  If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+   Original Author of file: David DURIEUX
+   Co-authors of file: 
+   Purpose of file:
+   Not translate this file, use https://www.transifex.net/projects/p/FusionInventory/
+   ----------------------------------------------------------------------
+ */
+");
 my @lines;
 while (my ($key, $po) = each %{$aref}) {
    @split = split /\|/ , $po->reference;
-   push @lines, "\$LANG['plugin_".$ARGV[0]."']['".$split[0]."'][".$split[1]."]=".$po->msgstr.";\n";
+   if (@split > 1) {
+      if ($po->msgstr eq '""' && $ARGV[2] eq '1') {
+         push @lines, "\$LANG['plugin_".$ARGV[0]."']['".$split[0]."'][".$split[1]."]=".$po->msgid.";\n";
+      } else {
+         push @lines, "\$LANG['plugin_".$ARGV[0]."']['".$split[0]."'][".$split[1]."]=".$po->msgstr.";\n";
+      }
+   }
 }
 my @out = sort @lines;
 my $before = '';
@@ -31,3 +91,6 @@ foreach my $line (@out) {
 }
 $fh->print("?>\n");
 $fh->close();
+}
+`rm -fr .tx`;
+`rm -fr translations`;
