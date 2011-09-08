c83edf905d8f6a3273fe24374487c32f94c2b38d
Alexandre Delaunay <adelaunay@teclib.com>
2011-09-08 10:40:27 +0200

    fix error on filepart http send

--- inc/filepart.class.php
+++ inc/filepart.class.php
@@ -106,7 +106,7 @@ class PluginFusinvdeployFilepart extends CommonDBTM {
 
          header('Content-Description: File Transfer');
          header('Content-Type: application/octet-stream');
-         header('Content-Disposition: attachment; filename='.$filename);
+         header('Content-Disposition: attachment; filename='.$sha512.'.gz');
          header('Content-Transfer-Encoding: binary');
          header('Expires: 0');
          header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
