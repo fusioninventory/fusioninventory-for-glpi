281de2e8d9e9f2ecf13d80afce0e290b8a0c7f3e
David Durieux <d.durieux@siprossii.com>
2011-10-09 10:51:12 +0200

    FIx add iprange on the wizard. closes #1169

--- front/iprange.form.php
+++ front/iprange.form.php
@@ -52,7 +52,7 @@ if (isset ($_POST["add"])) {
       $_POST['ip_end']    = $_POST['ip_end0'].".".$_POST['ip_end1'].".";
       $_POST['ip_end']   .= $_POST['ip_end2'].".".$_POST['ip_end3'];
       $newID = $iprange->add($_POST);
-      glpi_header(getItemTypeFormURL('PluginFusioninventoryIPRange')."?id=$newID");
+      glpi_header($_SERVER['HTTP_REFERER']);
    } else {
       glpi_header($_SERVER['HTTP_REFERER']);
    }
