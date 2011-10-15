304d6faa3ff3f7c695882c00f7f6169ee7e561e2
David Durieux <d.durieux@siprossii.com>
2011-10-15 10:10:51 +0200

    Update pre_require GLPI version for plugins installations

--- fusinvinventory/setup.php
+++ fusinvinventory/setup.php
@@ -157,15 +157,12 @@ function plugin_version_fusinvinventory() {
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_fusinvinventory_check_prerequisites() {
    global $LANG;
-   if (GLPI_VERSION >= '0.78') {
-      $plugin = new Plugin;
-      if (!$plugin->isActivated("fusioninventory")) {
-         return false;
-      }
-      return true;
-   } else {
-      echo $LANG['plugin_fusinvinventory']['errors'][50];
+   
+   if (version_compare(GLPI_VERSION,'0.80','lt') || version_compare(GLPI_VERSION,'0.81','ge')) {
+      echo $LANG['plugin_fusioninventory']['errors'][50];
+      return false;
    }
+   return true;
 }
 
 
