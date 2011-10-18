ea6c95f61b0bddd3590195fd4ffd95614d286270
Gonéri Le Bouder <goneri@rulezlan.org>
2011-10-18 10:05:42 +0200

    filesize must big a 64bit int
    closes: #1264

--- fusinvdeploy/install/mysql/plugin_fusinvdeploy-0.0.1-empty.sql
+++ fusinvdeploy/install/mysql/plugin_fusinvdeploy-0.0.1-empty.sql
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_fusinvdeploy_files` (
   `uncompress` tinyint(1) NOT NULL DEFAULT '0',
   `sha512` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `shortsha512` varchar(6) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `filesize` INT(32) NOT NULL DEFAULT  '0',
+  `filesize` INT(64) NOT NULL DEFAULT  '0',
   `plugin_fusinvdeploy_orders_id` INT(11) NOT NULL DEFAULT  '0',
 PRIMARY KEY (  `id` )
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
