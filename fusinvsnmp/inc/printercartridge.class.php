cb5d9bf538fb68a24e63359ce67a4df73ba024ff
Guillaume Rousse <Guillaume.Rousse@gmail.com>
2011-11-26 17:05:14 +0100

    use correct english sentence

--- fusinvsnmp/inc/printercartridge.class.php
+++ fusinvsnmp/inc/printercartridge.class.php
@@ -33,7 +33,7 @@
  */
 
 if (!defined('GLPI_ROOT')) {
-	die("Sorry. You can't access directly to this file");
+	die("Sorry. You can't access this file directly");
 }
 
 class PluginFusinvsnmpPrinterCartridge extends PluginFusinvsnmpCommonDBTM {
