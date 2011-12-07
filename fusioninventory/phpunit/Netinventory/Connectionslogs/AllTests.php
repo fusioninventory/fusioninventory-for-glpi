72cd860dc3fd92a0acea1fccd0fd02f190817904
David Durieux <d.durieux@siprossii.com>
2011-12-07 08:23:54 +0100

    Fix phpunit

--- fusioninventory/phpunit/Netinventory/Connectionslogs/AllTests.php
+++ fusioninventory/phpunit/Netinventory/Connectionslogs/AllTests.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-class Connectionlogs extends PHPUnit_Framework_TestCase {
+class Connectionslogs extends PHPUnit_Framework_TestCase {
 
 
    public function testSetModuleInventoryOff() {
@@ -320,11 +320,11 @@ Compiled Fri 25-Sep-09 08:49 by sasyamal</COMMENTS>
 
 
 
-class Connectionlogs_AllTests  {
+class Connectionslogs_AllTests  {
 
    public static function suite() {
 
-      $suite = new PHPUnit_Framework_TestSuite('Connectionlogs');
+      $suite = new PHPUnit_Framework_TestSuite('Connectionslogs');
       return $suite;
    }
 }
