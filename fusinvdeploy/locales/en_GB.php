fc2bb128b1c76eb02877fcc099143ad3ace098db
Tomas Abad <tabad@sescam.jccm.es>
2011-12-12 17:46:12 +0100

    en_GB translation: various improvements

--- fusinvdeploy/locales/en_GB.php
+++ fusinvdeploy/locales/en_GB.php
@@ -48,7 +48,7 @@ $LANG['plugin_fusinvdeploy']['title'][0]="$title";
 $LANG['plugin_fusinvdeploy']['massiveactions'][0]="Target a deployment task";
 
 $LANG['plugin_fusinvdeploy']['package'][0]="Actions";
-$LANG['plugin_fusinvdeploy']['package'][1]="Command";
+$LANG['plugin_fusinvdeploy']['package'][1]="Execute a command";
 $LANG['plugin_fusinvdeploy']['package'][2]="Launch (running file in package)";
 $LANG['plugin_fusinvdeploy']['package'][3]="Exécuter (system executable)";
 $LANG['plugin_fusinvdeploy']['package'][4]="Store";
@@ -63,19 +63,19 @@ $LANG['plugin_fusinvdeploy']['package'][12]="Files";
 $LANG['plugin_fusinvdeploy']['package'][13]="Actions";
 $LANG['plugin_fusinvdeploy']['package'][14]="Installation";
 $LANG['plugin_fusinvdeploy']['package'][15]="Uninstallation";
-$LANG['plugin_fusinvdeploy']['package'][16]="Package install";
+$LANG['plugin_fusinvdeploy']['package'][16]="Package deployment";
 $LANG['plugin_fusinvdeploy']['package'][17]="Package uninstall";
 $LANG['plugin_fusinvdeploy']['package'][18]="Move a file";
 $LANG['plugin_fusinvdeploy']['package'][19]="pieces of files";
 $LANG['plugin_fusinvdeploy']['package'][20]="Delete a file";
 $LANG['plugin_fusinvdeploy']['package'][21]="Show dialog";
 $LANG['plugin_fusinvdeploy']['package'][22]="Return codes";
-$LANG['plugin_fusinvdeploy']['package'][23]="One or more task(s) (#task#) uses this package, delete denied";
-$LANG['plugin_fusinvdeploy']['package'][24]="One or more task(s) (#task#) uses this package, edit denied";
+$LANG['plugin_fusinvdeploy']['package'][23]="One or more active tasks (#task#) use this package. Deletion denied.";
+$LANG['plugin_fusinvdeploy']['package'][24]="One or more active tasks (#task#) use this package. Edition denied.";
 $LANG['plugin_fusinvdeploy']['package'][25]="New name";
 $LANG['plugin_fusinvdeploy']['package'][26]="Add a package";
-$LANG['plugin_fusinvdeploy']['package'][27]="make a directory";
-$LANG['plugin_fusinvdeploy']['package'][28]="File copy";
+$LANG['plugin_fusinvdeploy']['package'][27]="Make a directory";
+$LANG['plugin_fusinvdeploy']['package'][28]="Copy a file";
 
 $LANG['plugin_fusinvdeploy']['files'][0]="Files management";
 $LANG['plugin_fusinvdeploy']['files'][1]="File name";
@@ -85,13 +85,13 @@ $LANG['plugin_fusinvdeploy']['files'][4]="File to upload";
 $LANG['plugin_fusinvdeploy']['files'][5]="Folder in package";
 $LANG['plugin_fusinvdeploy']['files'][6]="Maximum file size";
 $LANG['plugin_fusinvdeploy']['files'][7]="Upload From";
-$LANG['plugin_fusinvdeploy']['files'][8]="My computer";
+$LANG['plugin_fusinvdeploy']['files'][8]="This computer";
 $LANG['plugin_fusinvdeploy']['files'][9]="The server";
 
 $LANG['plugin_fusinvdeploy']['packagefiles'][0]="Files linked with package";
 
-$LANG['plugin_fusinvdeploy']['deploystatus'][0]="Deployment state";
-$LANG['plugin_fusinvdeploy']['deploystatus'][1]="associated logs";
+$LANG['plugin_fusinvdeploy']['deploystatus'][0]="Deployment status";
+$LANG['plugin_fusinvdeploy']['deploystatus'][1]="Associated logs";
 $LANG['plugin_fusinvdeploy']['deploystatus'][2]="The agent received the job request";
 $LANG['plugin_fusinvdeploy']['deploystatus'][3]="The agent started to check the mirror to download the file";
 $LANG['plugin_fusinvdeploy']['deploystatus'][4]="Preparing the working directory";
@@ -131,14 +131,14 @@ $LANG['plugin_fusinvdeploy']['form']['label'][16] = "From";
 $LANG['plugin_fusinvdeploy']['form']['label'][17] = "To";
 $LANG['plugin_fusinvdeploy']['form']['label'][18] = "Removal";
 $LANG['plugin_fusinvdeploy']['form']['label'][19] = "Uncompress";
-$LANG['plugin_fusinvdeploy']['form']['label'][20] = "transfer error: the file size is too big";
+$LANG['plugin_fusinvdeploy']['form']['label'][20] = "Transfer error: the file size is too big";
 $LANG['plugin_fusinvdeploy']['form']['label'][21] = "Filesize";
 $LANG['plugin_fusinvdeploy']['form']['label'][22] = "Failed to copy file";
 
 $LANG['plugin_fusinvdeploy']['form']['action'][0] = "Add";
 $LANG['plugin_fusinvdeploy']['form']['action'][1] = "Delete";
 $LANG['plugin_fusinvdeploy']['form']['action'][2] = "OK";
-$LANG['plugin_fusinvdeploy']['form']['action'][3] = "Select your file";
+$LANG['plugin_fusinvdeploy']['form']['action'][3] = "Select the file";
 $LANG['plugin_fusinvdeploy']['form']['action'][4] = "File saved!";
 $LANG['plugin_fusinvdeploy']['form']['action'][5] = "Or URL";
 $LANG['plugin_fusinvdeploy']['form']['action'][6] = "Add return code";
@@ -175,14 +175,14 @@ $LANG['plugin_fusinvdeploy']['form']['check'][2] = "Register key value";
 $LANG['plugin_fusinvdeploy']['form']['check'][3] = "File exist";
 $LANG['plugin_fusinvdeploy']['form']['check'][4] = "File missing";
 $LANG['plugin_fusinvdeploy']['form']['check'][5] = "File size";
-$LANG['plugin_fusinvdeploy']['form']['check'][6] = "Hash512 of file";
+$LANG['plugin_fusinvdeploy']['form']['check'][6] = "SHA-512 hash value";
 $LANG['plugin_fusinvdeploy']['form']['check'][7] = "Free space";
-$LANG['plugin_fusinvdeploy']['form']['check'][8] = "Filesize equal";
-$LANG['plugin_fusinvdeploy']['form']['check'][9] = "Filesize lower";
+$LANG['plugin_fusinvdeploy']['form']['check'][8] = "Filesize equal to";
+$LANG['plugin_fusinvdeploy']['form']['check'][9] = "Filesize lower than";
 
-$LANG['plugin_fusinvdeploy']['form']['mirror'][1] = "Mirror";
-$LANG['plugin_fusinvdeploy']['form']['mirror'][2] = "Mirrors";
-$LANG['plugin_fusinvdeploy']['form']['mirror'][3] = "Address of the mirror";
+$LANG['plugin_fusinvdeploy']['form']['mirror'][1] = "Mirror servers";
+$LANG['plugin_fusinvdeploy']['form']['mirror'][2] = "Mirror servers";
+$LANG['plugin_fusinvdeploy']['form']['mirror'][3] = "Mirror server address";
 
 $LANG['plugin_fusinvdeploy']['form']['command_status'][0] = "Make your choice...";
 $LANG['plugin_fusinvdeploy']['form']['command_status'][1] = "Type";
@@ -200,7 +200,7 @@ $LANG['plugin_fusinvdeploy']['form']['action_message'][3] = "Type";
 $LANG['plugin_fusinvdeploy']['form']['action_message'][4] = "Informations";
 $LANG['plugin_fusinvdeploy']['form']['action_message'][5] = "report of the install";
 
-$LANG['plugin_fusinvdeploy']['task'][0] = "Deployement task";
+$LANG['plugin_fusinvdeploy']['task'][0] = "Deployment tasks";
 $LANG['plugin_fusinvdeploy']['task'][1] = "Task";
 $LANG['plugin_fusinvdeploy']['task'][3] = "Add task";
 $LANG['plugin_fusinvdeploy']['task'][5] = "Task";
@@ -217,11 +217,11 @@ $LANG['plugin_fusinvdeploy']['task'][18] = "---";
 $LANG['plugin_fusinvdeploy']['task'][19] = "Edit impossible, this task is active";
 $LANG['plugin_fusinvdeploy']['task'][20] = "This task is active. delete denied";
 
-$LANG['plugin_fusinvdeploy']['group'][0] = "Sets";
-$LANG['plugin_fusinvdeploy']['group'][1] = "Static set";
-$LANG['plugin_fusinvdeploy']['group'][2] = "dynamic set";
-$LANG['plugin_fusinvdeploy']['group'][3] = "Set";
-$LANG['plugin_fusinvdeploy']['group'][4] = "Add set";
+$LANG['plugin_fusinvdeploy']['group'][0] = "Groups of computers";
+$LANG['plugin_fusinvdeploy']['group'][1] = "Static group";
+$LANG['plugin_fusinvdeploy']['group'][2] = "Dynamic group";
+$LANG['plugin_fusinvdeploy']['group'][3] = "Group of computers";
+$LANG['plugin_fusinvdeploy']['group'][4] = "Add group";
 $LANG['plugin_fusinvdeploy']['group'][5] = "Si aucune ligne de la liste n'est selectionné, le champs texte de gauche sera utilisé pour la recherche";
 
 ?>
