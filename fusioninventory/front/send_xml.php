14a861b1603ab6c98eb7255162fdae770f18ba79
David Durieux <d.durieux@siprossii.com>
2011-12-21 15:25:44 +0100

    Fix script file for download Computers XML

--- fusioninventory/front/send_xml.php
+++ fusioninventory/front/send_xml.php
@@ -29,7 +29,7 @@
 
    @package   FusionInventory
    @author    Vincent Mazzoni
-   @co-author 
+   @co-author David Durieux
    @copyright Copyright (c) 2010-2011 FusionInventory team
    @license   AGPL License 3.0 or (at your option) any later version
               http://www.gnu.org/licenses/agpl-3.0-standalone.html
@@ -42,6 +42,7 @@
 
 if (!defined('GLPI_ROOT')) {
    define('GLPI_ROOT', '../../..');
+   include (GLPI_ROOT . "/inc/includes.php");
 }
 if (!defined("GLPI_PLUGIN_DOC_DIR")){
    define("GLPI_PLUGIN_DOC_DIR",GLPI_ROOT . "/files/_plugins");
