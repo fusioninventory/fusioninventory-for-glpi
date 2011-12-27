88c0ce735f3485e4df0858186351c4b5d37a396d
David Durieux <d.durieux@siprossii.com>
2011-12-27 15:20:40 +0100

    Fix sql error

--- fusioninventory/inc/agentmodule.class.php
+++ fusioninventory/inc/agentmodule.class.php
@@ -287,6 +287,8 @@ class PluginFusioninventoryAgentmodule extends CommonDBTM {
             foreach ($a_agentList as $agent_id) {
                if (($items_id != '0') AND ($items_id == $agent_id)) {
                   return true;
+               } else if ($items_id != '0')) {
+                  return array();  
                }
                if ($i> 0) {
                   $sep  = ',';
