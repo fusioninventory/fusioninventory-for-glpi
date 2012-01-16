77280d400be39997b20a0cd56311ac95f6c89e64
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2012-01-16 12:14:30 +0100

    packages : display label in ret check closes : #1256

--- fusinvdeploy/js/package_action.front.php
+++ fusinvdeploy/js/package_action.front.php
@@ -29,14 +29,14 @@
 
    @package   FusionInventory
    @author    Alexandre Delaunay
-   @co-author 
+   @co-author
    @copyright Copyright (c) 2010-2011 FusionInventory team
    @license   AGPL License 3.0 or (at your option) any later version
               http://www.gnu.org/licenses/agpl-3.0-standalone.html
    @link      http://www.fusioninventory.org/
    @link      http://forge.fusioninventory.org/projects/fusioninventory-for-glpi/
    @since     2010
- 
+
    ------------------------------------------------------------------------
  */
 
@@ -348,6 +348,15 @@ var {$render}actionGridProxyRetChecks = {
                header: '{$LANG['plugin_fusinvdeploy']['form']['command_status'][1]}',
                dataIndex: 'type',
                width: 180,
+               renderer: function(val) {
+                  switch(val) {
+                     case 'RETURNCODE_OK': return '{$LANG['plugin_fusinvdeploy']['form']['command_status'][3]}';
+                     case 'RETURNCODE_KO': return '{$LANG['plugin_fusinvdeploy']['form']['command_status'][4]}';
+                     case 'REGEX_OK': return '{$LANG['plugin_fusinvdeploy']['form']['command_status'][5]}';
+                     case 'REGEX_KO': return '{$LANG['plugin_fusinvdeploy']['form']['command_status'][6]}';
+                  }
+                  return "";
+               },
                editor: new Ext.form.ComboBox({
                   triggerAction: 'all',
                   name: 'type',
