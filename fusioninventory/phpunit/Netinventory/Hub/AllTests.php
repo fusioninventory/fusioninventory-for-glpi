649548307465621e0df26f9cc58f5c36688a33b9
David Durieux <d.durieux@siprossii.com>
2012-04-22 10:53:50 +0200

    Update unit tests

--- fusioninventory/phpunit/Netinventory/Hub/AllTests.php
+++ fusioninventory/phpunit/Netinventory/Hub/AllTests.php
@@ -543,7 +543,7 @@ Compiled Fri 25-Sep-09 08:49 by sasyamal</COMMENTS>
       echo $code."\n";
       $this->assertEquals($code, '<?xml version="1.0" encoding="UTF-8"?>
 <REPLY>
-</REPLY>
+   </REPLY>
 ', 'Return code not right');
       
       $GLPIlog = new GLPIlogs();
