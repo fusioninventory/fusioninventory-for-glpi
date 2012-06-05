753be561c4f17d4c025be4e72fe54e2f093369ed
David Durieux <d.durieux@siprossii.com>
2012-06-05 08:37:16 +0200

    Change version of plugins to 0.80+1.4

--- fusinvinventory/setup.php
+++ fusinvinventory/setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_FUSINVINVENTORY_VERSION","0.80+1.3");
+define ("PLUGIN_FUSINVINVENTORY_VERSION","0.80+1.4");
 
 // Init the hooks of fusinvdeploy
 function plugin_init_fusinvinventory() {
