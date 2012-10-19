d8be1f089b6acf591c1a685d17e4bb6f6961133d
Alexandre <adelaunay@teclib.com>
2012-10-19 11:34:33 +0200

    add softwarelicenses_id to licenceinfos table schema

--- fusinvinventory/install/mysql/plugin_fusinvinventory-0.83+2.0-empty.sql
+++ fusinvinventory/install/mysql/plugin_fusinvinventory-0.83+2.0-empty.sql
@@ -165,6 +165,7 @@ DROP TABLE IF EXISTS `glpi_plugin_fusinvinventory_licenseinfos`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_fusinvinventory_licenseinfos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `computers_id` int(11) NOT NULL DEFAULT '0',
+  `softwarelicenses_id` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `fullname` varchar(255) DEFAULT NULL,
   `key` varchar(255) DEFAULT NULL,
