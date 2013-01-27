11408d8961635be4411d790a9d4354ab6b75fa1d
David Durieux <d.durieux@siprossii.com>
2013-01-27 15:57:53 +0100

    Add unit test for blacklist criteria

--- fusioninventory/phpunit/FusinvInstall/AllTests.php
+++ fusioninventory/phpunit/FusinvInstall/AllTests.php
@@ -49,45 +49,46 @@ class FusinvInstall extends PHPUnit_Framework_TestCase {
          return;
       }
 
-       $comparaisonSQLFile = "plugin_".$pluginname."-empty.sql";
-       // See http://joefreeman.co.uk/blog/2009/07/php-script-to-compare-mysql-database-schemas/
+      $comparaisonSQLFile = "plugin_".$pluginname."-empty.sql";
+      // See http://joefreeman.co.uk/blog/2009/07/php-script-to-compare-mysql-database-schemas/
 
-       $file_content = file_get_contents("../../".$pluginname."/install/mysql/".$comparaisonSQLFile);
-       $a_lines = explode("\n", $file_content);
+      $file_content = file_get_contents("../../".$pluginname."/install/mysql/".$comparaisonSQLFile);
+      $a_lines = explode("\n", $file_content);
 
-       $a_tables_ref = array();
-       $current_table = '';
-       foreach ($a_lines as $line) {
-          if (strstr($line, "CREATE TABLE ")
-                  OR strstr($line, "CREATE VIEW")) {
-             $matches = array();
-             preg_match("/`(.*)`/", $line, $matches);
-             $current_table = $matches[1];
-          } else {
-             if (preg_match("/^`/", trim($line))) {
-                $s_line = explode("`", $line);
-                $s_type = explode("COMMENT", $s_line[2]);
-                $s_type[0] = trim($s_type[0]);
-                $s_type[0] = str_replace(" COLLATE utf8_unicode_ci", "", $s_type[0]);
-                $s_type[0] = str_replace(" CHARACTER SET utf8", "", $s_type[0]);
-                $a_tables_ref[$current_table][$s_line[1]] = str_replace(",", "", $s_type[0]);
-             }
-          }
-       }
+      $a_tables_ref = array();
+      $current_table = '';
+      foreach ($a_lines as $line) {
+         if (strstr($line, "CREATE TABLE ")
+                 OR strstr($line, "CREATE VIEW")) {
+            $matches = array();
+            preg_match("/`(.*)`/", $line, $matches);
+            $current_table = $matches[1];
+         } else {
+            if (preg_match("/^`/", trim($line))) {
+               $s_line = explode("`", $line);
+               $s_type = explode("COMMENT", $s_line[2]);
+               $s_type[0] = trim($s_type[0]);
+               $s_type[0] = str_replace(" COLLATE utf8_unicode_ci", "", $s_type[0]);
+               $s_type[0] = str_replace(" CHARACTER SET utf8", "", $s_type[0]);
+               $a_tables_ref[$current_table][$s_line[1]] = str_replace(",", "", $s_type[0]);
+            }
+         }
+      }
 
-      // * Get tables from MySQL
-      $a_tables_db = array();
-      $a_tables = array();
-      // SHOW TABLES;
-      $query = "SHOW TABLES";
-      $result = $DB->query($query);
-      while ($data=$DB->fetch_array($result)) {
-         if ((strstr($data[0], "tracker")
-                 OR strstr($data[0], $pluginname))
-             AND(!strstr($data[0], "glpi_plugin_fusioninventory_pcidevices"))
-             AND(!strstr($data[0], "glpi_plugin_fusioninventory_pcivendors"))
-             AND(!strstr($data[0], "glpi_plugin_fusioninventory_usbdevices"))
-             AND(!strstr($data[0], "glpi_plugin_fusioninventory_usbvendors"))){
+     // * Get tables from MySQL
+     $a_tables_db = array();
+     $a_tables = array();
+     // SHOW TABLES;
+     $query = "SHOW TABLES";
+     $result = $DB->query($query);
+     while ($data=$DB->fetch_array($result)) {
+        if ((strstr($data[0], "tracker")
+                OR strstr($data[0], $pluginname))
+            AND(!strstr($data[0], "glpi_plugin_fusioninventory_pcidevices"))
+            AND(!strstr($data[0], "glpi_plugin_fusioninventory_pcivendors"))
+            AND(!strstr($data[0], "glpi_plugin_fusioninventory_usbdevices"))
+            AND(!strstr($data[0], "glpi_plugin_fusioninventory_usbvendors"))){
+           
             $data[0] = str_replace(" COLLATE utf8_unicode_ci", "", $data[0]);
             $data[0] = str_replace("( ", "(", $data[0]);
             $data[0] = str_replace(" )", ")", $data[0]);
@@ -267,8 +268,17 @@ class FusinvInstall extends PHPUnit_Framework_TestCase {
       // TODO : test glpi_displaypreferences, rules, bookmark...
 
 
-      if ($pluginname == 'fusinvsnmp') {
+      /*
+       * Verify table glpi_plugin_fusioninventory_inventorycomputercriterias 
+       * have right 10 lines
+       */
+      $query = "SELECT `id` FROM `glpi_plugin_fusioninventory_inventorycomputercriterias`";
+      $result = $DB->query($query);
+      $this->assertEquals($DB->numrows($result), 10, "Number of criteria not right in table".
+              " glpi_plugin_fusioninventory_inventorycomputercriterias ".$when);
 
+      
+      
       /*
        * Verify SNMP models have a right itemtype
        */
@@ -288,7 +298,6 @@ class FusinvInstall extends PHPUnit_Framework_TestCase {
       $this->assertEquals($DB->numrows($result), 0, "SNMP models are in double (name of models)");
 
 
-      }
 
    }
 }
