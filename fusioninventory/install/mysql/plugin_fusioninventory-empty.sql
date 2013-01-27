fb444b9c2b0a33177077eddd49098e5c2a472dfb
David Durieux <d.durieux@siprossii.com>
2013-01-27 14:24:46 +0100

    FIx sql

--- fusioninventory/install/mysql/plugin_fusioninventory-empty.sql
+++ fusioninventory/install/mysql/plugin_fusioninventory-empty.sql
@@ -1127,7 +1127,7 @@ INSERT INTO `glpi_plugin_fusioninventory_inventorycomputerblacklists`
 (57, 3, '00:50:56:C0:00:08'),
 (58, 3, '02:80:37:EC:02:00'),
 (59, 1, 'MB-1234567890'),
-(60, 1, '0';
+(60, 1, '0');
 
 
 
