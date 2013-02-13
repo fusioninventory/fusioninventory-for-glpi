2f76a445f5d140cf17028ce94759823f3ccb9f5f
David Durieux <d.durieux@siprossii.com>
2013-02-13 01:05:01 +0100

    Fix software entity

--- inc/formatconvert.class.php
+++ inc/formatconvert.class.php
@@ -997,9 +997,7 @@ class PluginFusioninventoryFormatconvert {
    function computerSoftwareTransformation($a_inventory, $entities_id) {
       
       $entities_id_software = Entity::getUsedConfig('entities_id_software', 
-                                                    $_SESSION["plugin_fusinvinventory_entity"], 
-                                                    '', 
-                                                    TRUE);
+                                                    $_SESSION["plugin_fusinvinventory_entity"]);
       
       $nb_RuleDictionnarySoftware = countElementsInTable("glpi_rules", 
                                                          "`sub_type`='RuleDictionnarySoftware'
