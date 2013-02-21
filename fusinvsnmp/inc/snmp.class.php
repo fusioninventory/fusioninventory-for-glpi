c499443fd8019192c57f9c56ceb8d45c9da620a6
David Durieux <d.durieux@siprossii.com>
2013-02-21 11:28:02 +0100

    Fix search IP device for Phone use CDP/LLDP and not have MODEL defined

--- fusinvsnmp/inc/snmp.class.php
+++ fusinvsnmp/inc/snmp.class.php
@@ -138,7 +138,8 @@ class PluginFusinvsnmpSNMP extends CommonDBTM {
       
       // Detect IP Phone
       if ($PortID == "") {
-         if (strstr($model, "Phone")) {
+         if (strstr($model, "Phone")
+               || $model == '') {
             $queryPort = "SELECT glpi_networkports.*
                            FROM `glpi_phones`
                               LEFT JOIN `glpi_networkports`
