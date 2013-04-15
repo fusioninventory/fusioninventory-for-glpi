20f751df859f01497304bfe8c87332744565539e
Kevin Roy <kiniou@gmail.com>
2013-04-15 16:19:00 +0200

    add new files for location rules

---
+++ inc/inventoryrulelocation.class.php
@@ -0,0 +1,205 @@
+<?php
+
+/*
+   ------------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2013 by the FusionInventory Development Team.
+
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of FusionInventory project.
+
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with FusionInventory. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   FusionInventory
+   @author    Walid Nouh
+   @co-author David Durieux
+   @copyright Copyright (c) 2010-2013 FusionInventory team
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      http://www.fusioninventory.org/
+   @link      http://forge.fusioninventory.org/projects/fusioninventory-for-glpi/
+   @since     2010
+
+   ------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class PluginFusioninventoryInventoryRuleLocation extends Rule {
+
+   // From Rule
+   //public $right='rule_import';
+   public $can_sort=TRUE;
+   public $specific_parameters = FALSE;
+
+
+
+   function getTitle() {
+      return __('Location rules', 'fusioninventory');
+
+   }
+
+
+
+   function preProcessPreviewResults($output) {
+      return $output;
+   }
+
+
+
+   function maxActionsCount() {
+      // Unlimited
+      return 2;
+   }
+
+
+
+   function executeActions($output, $params) {
+
+      PluginFusioninventoryToolbox::logIfExtradebug(
+         "pluginFusioninventory-locationrules",
+         "execute action\n"
+      );
+
+      if (count($this->actions)) {
+         foreach ($this->actions as $action) {
+            switch ($action->fields["action_type"]) {
+               case "assign" :
+                  PluginFusioninventoryToolbox::logIfExtradebug(
+                     "pluginFusioninventory-locationrules",
+                     "value ".$action->fields["value"]."\n"
+                  );
+                  $output[$action->fields["field"]] = $action->fields["value"];
+                  break;
+
+               case "regex_result" :
+                  //Assign entity using the regex's result
+                  if ($action->fields["field"] == "_affect_entity_by_tag") {
+                     PluginFusioninventoryToolbox::logIfExtradebug(
+                        "pluginFusioninventory-locationrules",
+                        "value ".$action->fields["value"]."\n"
+                     );
+                     //Get the TAG from the regex's results
+                     $res = RuleAction::getRegexResultById($action->fields["value"],
+                                                           $this->regex_results[0]);
+                     if (!is_null($res)) {
+                        //Get the entity associated with the TAG
+                        $target_entity = Entity::getEntityIDByTag($res);
+                        if ($target_entity != '') {
+                           $output["locations_id"]=$target_entity;
+                        }
+                     }
+                  }
+                  break;
+            }
+         }
+      }
+      return $output;
+   }
+
+
+
+   function getCriterias() {
+
+      $criterias = array ();
+
+      $criterias['tag']['field']     = 'name';
+      $criterias['tag']['name']      = __('FusionInventory tag', 'fusioninventory');
+
+
+      $criterias['domain']['field']     = 'name';
+      $criterias['domain']['name']      = __('Domain');
+
+
+      $criterias['subnet']['field']     = 'name';
+      $criterias['subnet']['name']      = __('Subnet');
+
+      $criterias['ip']['field']     = 'name';
+      $criterias['ip']['name']      = __('Address')." ".__('IP');
+
+
+      $criterias['name']['field']     = 'name';
+      $criterias['name']['name']      = __('Computer\'s name');
+
+
+      $criterias['serial']['field']     = 'name';
+      $criterias['serial']['name']      = __('Serial Number');
+
+
+      return $criterias;
+   }
+
+
+
+   function getActions() {
+
+      $actions = array();
+
+      $actions['locations_id']['name']  = __('Location');
+
+      $actions['locations_id']['type']  = 'dropdown';
+      $actions['locations_id']['table'] = 'glpi_locations';
+/*
+      $actions['_affect_entity_by_tag']['name'] = __('Entity from TAG');
+
+      $actions['_affect_entity_by_tag']['type'] = 'text';
+      $actions['_affect_entity_by_tag']['force_actions'] = array('regex_result');
+*/
+      $actions['_ignore_import']['name'] =
+                     __('Ignore in FusionInventory import', 'fusioninventory');
+
+      $actions['_ignore_import']['type'] = 'yesonly';
+
+      return $actions;
+   }
+
+
+
+   function displayAdditionalRuleCondition($condition, $criteria, $name, $value, $test=FALSE) {
+      if ($test) {
+         return FALSE;
+      }
+
+      switch ($condition) {
+         case Rule::PATTERN_FIND:
+            return FALSE;
+            break;
+
+         case PluginFusioninventoryInventoryRuleImport::PATTERN_IS_EMPTY :
+            Dropdown::showYesNo($name, 0, 0);
+            return TRUE;
+
+         case Rule::PATTERN_EXISTS:
+            echo Dropdown::showYesNo($name, 1, 0);
+            return TRUE;
+
+         case Rule::PATTERN_DOES_NOT_EXISTS:
+            echo Dropdown::showYesNo($name, 1, 0);
+            return TRUE;
+
+      }
+
+      return FALSE;
+   }
+}
+
+?>
