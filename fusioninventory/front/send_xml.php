da0cbd353c52c648605f6339766c58b4d62747af
David Durieux <d.durieux@siprossii.com>
2013-07-12 13:46:29 +0200

    Fixcopyright and licence

--- fusioninventory/front/send_xml.php
+++ fusioninventory/front/send_xml.php
@@ -23,14 +23,14 @@
    GNU Affero General Public License for more details.
 
    You should have received a copy of the GNU Affero General Public License
-   along with Behaviors. If not, see <http://www.gnu.org/licenses/>.
+   along with FusionInventory. If not, see <http://www.gnu.org/licenses/>.
 
    ------------------------------------------------------------------------
 
    @package   FusionInventory
    @author    Vincent Mazzoni
    @co-author David Durieux
-   @copyright Copyright (c) 2010-2012 FusionInventory team
+   @copyright Copyright (c) 2010-2013 FusionInventory team
    @license   AGPL License 3.0 or (at your option) any later version
               http://www.gnu.org/licenses/agpl-3.0-standalone.html
    @link      http://www.fusioninventory.org/
