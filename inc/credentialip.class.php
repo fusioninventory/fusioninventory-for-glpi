a6b826f4db38079f51fb6f9c19be5e0065b0d804
David Durieux <d.durieux@siprossii.com>
2013-12-24 18:41:08 +0100

    Fix rights

--- inc/credentialip.class.php
+++ inc/credentialip.class.php
@@ -49,21 +49,10 @@ class PluginFusioninventoryCredentialIp extends CommonDropdown {
    public $first_level_menu  = "plugins";
    public $second_level_menu = "fusioninventory";
 
+   static $rightname = 'plugin_fusioninventory_credentialip';
    
    static function getTypeName($nb=0) {
       return __('Remote device inventory', 'fusioninventory');
-   }
-
-   
-   
-   static function canCreate() {
-      return PluginFusioninventoryProfile::haveRight('credentialip', 'w');
-   }
-
-   
-   
-   static function canView() {
-      return PluginFusioninventoryProfile::haveRight('credentialip', 'r');
    }
 
 
