a613cde858ec9adac2ac33ed9a0a825be5da4032
David Durieux <d.durieux@siprossii.com>
2014-05-09 21:25:11 +0200

    Fix phpunit

--- phpunit/1_Unit/ComputerUpdateTest.php
+++ phpunit/1_Unit/ComputerUpdateTest.php
@@ -355,7 +355,8 @@ class ComputerUpdateTest extends RestoreDatabase_TestCase {
           'winowner'                                  => 'test',
           'wincompany'                                => 'siprossii',
           'remote_addr'                               => NULL,
-          'plugin_fusioninventory_computerarchs_id'   => 0
+          'plugin_fusioninventory_computerarchs_id'   => 0,
+          'is_entitylocked'                           => 0
       );
 
       $this->assertEquals($a_reference, $a_computer);
