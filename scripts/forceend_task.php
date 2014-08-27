6de59269df8d207bff5f5b725338174b37540db2
David Durieux <d.durieux@siprossii.com>
2014-08-28 00:11:24 +0200

    Add script to force end task. closes #2680

---
+++ scripts/forceend_task.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+   ------------------------------------------------------------------------
+   FusionInventory
+   Copyright (C) 2010-2014 by the FusionInventory Development Team.
+
+   http://www.fusioninventory.org/   http://forge.fusioninventory.org/
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of FusionInventory project.
+
+   FusionInventory is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   FusionInventory is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with FusionInventory. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   FusionInventory
+   @author    David Durieux
+   @co-author
+   @copyright Copyright (c) 2010-2014 FusionInventory team
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      http://www.fusioninventory.org/
+   @link      http://forge.fusioninventory.org/projects/fusioninventory-for-glpi/
+   @since     2014
+
+   ------------------------------------------------------------------------
+ */
+
+if (!isset($_SERVER['argv'][1])) {
+   die("usage: ".$_SERVER['argv'][0]." tasks_id\n");
+}
+
+chdir(dirname($_SERVER["SCRIPT_FILENAME"]));
+
+include ("../../../inc/includes.php");
+
+// Init debug variable
+$_SESSION['glpi_use_mode'] = Session::DEBUG_MODE;
+$_SESSION['glpilanguage']  = "en_GB";
+
+Session::LoadLanguage();
+
+// Only show errors
+$CFG_GLPI["debug_sql"]        = $CFG_GLPI["debug_vars"] = 0;
+$CFG_GLPI["use_log_in_files"] = 1;
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+//set_error_handler('userErrorHandlerDebug');
+
+$DB = new DB();
+if (!$DB->connected) {
+   die("No DB connection\n");
+}
+
+$pfTask = new PluginFusioninventoryTask();
+$pfTaskjob = new PluginFusioninventoryTaskjob();
+
+if ($pfTask->getFromDB($_SERVER['argv'][1])) {
+   $a_taskjobs = $pfTaskjob->find("`plugin_fusioninventory_tasks_id`='".$_SERVER['argv'][1]."'");
+   foreach ($a_taskjobs as $data) {
+      $pfTaskjob->getFromDB($data['id']);
+      $pfTaskjob->forceEnd();
+   }
+}
+
