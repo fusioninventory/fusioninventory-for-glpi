a36d878a4ddee36e67e7ab361bf1e5ff23424f70
Kevin Roy <kiniou@gmail.com>
2014-10-13 19:08:25 +0200

    changed some common mysql functions
    - renamed construct_mysql() to construct_mysql_options().
    - added mysql_dump() function in order to simplify SaveInstallTest.

--- phpunit/0_Install/SaveInstallTest.php
+++ phpunit/0_Install/SaveInstallTest.php
@@ -14,26 +14,8 @@ class SaveInstallTest extends PHPUnit_Framework_TestCase {
       include_once (GLPI_ROOT . "/inc/dbmysql.class.php");
       include_once (GLPI_CONFIG_DIR . "/config_db.php");
       $DB = new DB();
-      $mysqldump_cmd = array('mysqldump');
 
-      $mysqldump_cmd[] = "--opt";
-      $mysqldump_cmd[] = "-h ".$DB->dbhost;
-
-      $mysqldump_cmd[] = "-u ".$DB->dbuser;
-
-      if (!empty($DB->dbpassword)) {
-         $mysqldump_cmd[] = "-p'".urldecode($DB->dbpassword)."'";
-      }
-
-      $mysqldump_cmd[] = $DB->dbdefault;
-
-      $output = shell_exec(
-         implode(' ', $mysqldump_cmd)
-      );
-      $this->assertNotNull($output, print_r(implode(' ', $mysqldump_cmd),TRUE));
-      $dumpfile = fopen("./save.sql", "w+");
-      fwrite($dumpfile, $output);
-      fclose($dumpfile);
+      mysql_dump($DB->dbuser, $DB->dbhost, $DB->dbpassword, $DB->dbdefault, './save.sql');
 
       $this->assertFileExists("./save.sql");
       $filestats = stat("./save.sql");
