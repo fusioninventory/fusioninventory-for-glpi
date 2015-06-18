96393e42299564bc26095bb44727c9625676751f
Alexandre Delaunay <adelaunay@teclib.com>
2015-06-18 11:30:49 +0200

    unlink computer from agent

--- inc/agent.class.php
+++ inc/agent.class.php
@@ -353,7 +353,7 @@ class PluginFusioninventoryAgent extends CommonDBTM {
    *
    **/
    function showForm($computers_id, $options=array()) {
-
+      global $CFG_GLPI;
 
       if ($computers_id!='') {
          $this->getFromDB($computers_id);
@@ -394,6 +394,10 @@ class PluginFusioninventoryAgent extends CommonDBTM {
          echo $oComputer->getLink(1);
          echo Html::hidden('computers_id',
                            array('value' => $this->fields["computers_id"]));
+         echo "&nbsp;<a class='pointer' onclick='submitGetLink(\"".
+               $CFG_GLPI['root_doc']."/plugins/fusioninventory/front/agent.form.php\", ".
+               "{\"update\": \"update\", \"computers_id\": 0, \"id\": ".$this->fields['id']."});'>".
+               "<img src='".$CFG_GLPI['root_doc']."/pics/delete.png' /></a>";
       } else {
          Computer_Item::dropdownConnect("Computer", "Computer", 'computers_id',
                                         $_SESSION['glpiactive_entity']);
