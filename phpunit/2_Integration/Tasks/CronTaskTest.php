53608ba2da9f25fc1f0f008f47f613bbc7e6d28b
David Durieux <d.durieux@siprossii.com>
2015-07-15 02:04:00 +0200

    Fix phpunit

--- phpunit/2_Integration/Tasks/CronTaskTest.php
+++ phpunit/2_Integration/Tasks/CronTaskTest.php
@@ -272,7 +272,6 @@ class CronTaskTest extends RestoreDatabase_TestCase {
    public function prepareTaskDisabled() {
       global $DB;
 
-      // Verify cancel agent prepared when one computer not verify dynamic group in deploy task
       $DB->connect();
 
       $pfTask = new PluginFusioninventoryTask();
@@ -283,22 +282,15 @@ class CronTaskTest extends RestoreDatabase_TestCase {
 
       PluginFusioninventoryTask::cronTaskscheduler();
 
-      $pfTask = new PluginFusioninventoryTask();
-
       $data = $pfTask->getJoblogs(array(1));
 
-      $ref = array(
-          1 => 'portdavid',
-          2 => 'computer2',
-          3 => 'computer3',
-          4 => 'computer4'
-      );
+      $ref = array();
 
-      $this->assertEquals($ref, $data['agents']);
+      $this->assertEquals($ref, $data['agents'], 'Task inactive, so no agent prepared');
 
       $ref_prepared = array();
 
-      $this->assertEquals($ref_prepared, $data['tasks'][1]['jobs'][1]['targets']['PluginFusioninventoryDeployPackage_1']['counters']['agents_prepared']);
+      $this->assertEquals($ref_prepared, $data['tasks']);
    }
 
 
