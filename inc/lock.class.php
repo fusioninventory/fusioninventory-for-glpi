ee371650b70934100fba47a903eb3aa4fc984858
Alexandre Delaunay <adelaunay@teclib.com>
2015-12-08 12:07:33 +0100

    retrieve inventory data in case of delete last lock on a item

--- inc/lock.class.php
+++ inc/lock.class.php
@@ -749,6 +749,12 @@ class PluginFusioninventoryLock extends CommonDBTM{
             }
          }
       }
+
+      //delete all lock case (no more lock)
+      if (!isset($item->updates)) {
+         $a_fieldList = array();
+      }
+
       $item_device = new $itemtype();
       $item_device->getFromDB($items_id);
       $a_serialized = $pfLock->getSerializedInventoryArray($itemtype, $items_id);
