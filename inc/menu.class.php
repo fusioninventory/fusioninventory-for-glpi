4b46904941716bb233a9814aeed4106a258f153d
Alexandre Delaunay <adelaunay@teclib.com>
2016-01-22 16:40:06 +0100

    remove inner join

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -777,10 +777,11 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       $restrict_entity    = getEntitiesRestrictRequest(" AND", 'comp');
       $query_fi_computers = "SELECT COUNT(comp.`id`) as nb_computers
                              FROM glpi_computers comp
-                             INNER JOIN glpi_plugin_fusioninventory_inventorycomputercomputers fi_comp
+                             LEFT JOIN glpi_plugin_fusioninventory_inventorycomputercomputers fi_comp
                                ON fi_comp.`computers_id` = comp.`id`
                              WHERE comp.`is_deleted`  = '0' 
                                AND comp.`is_template` = '0'
+                               comp.`id` IS NOT NULL
                                $restrict_entity";
       $res_fi_computers = $DB->query($query_fi_computers);
       if ($data_fi_computers = $DB->fetch_assoc($res_fi_computers)) {
@@ -809,10 +810,11 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       $restrict_entity  = getEntitiesRestrictRequest(" AND", 'net');
       $query_fi_net = "SELECT COUNT(net.`id`) as nb_net
                              FROM glpi_networkequipments net
-                             INNER JOIN glpi_plugin_fusioninventory_networkequipments fi_net
+                             LEFT JOIN glpi_plugin_fusioninventory_networkequipments fi_net
                                ON fi_net.`networkequipments_id` = net.`id`
                              WHERE net.`is_deleted`  = '0' 
                                AND net.`is_template` = '0'
+                               AND net.`id` IS NOT NULL
                                $restrict_entity";
       $res_fi_net = $DB->query($query_fi_net);
       if ($data_fi_net = $DB->fetch_assoc($res_fi_net)) {
@@ -823,10 +825,11 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       $restrict_entity = getEntitiesRestrictRequest(" AND", 'printers');
       $query_fi_printers = "SELECT COUNT(printers.`id`) as nb_printers
                              FROM glpi_printers printers
-                             INNER JOIN glpi_plugin_fusioninventory_printers fi_printer
+                             LEFT JOIN glpi_plugin_fusioninventory_printers fi_printer
                                ON fi_printer.`printers_id` = printers.`id`
                              WHERE printers.`is_deleted`  = '0' 
                                AND printers.`is_template` = '0'
+                               AND printers.`id` IS NOT NULL
                                $restrict_entity";
       $res_fi_printers = $DB->query($query_fi_printers);
       if ($data_fi_printers = $DB->fetch_assoc($res_fi_printers)) {
@@ -851,9 +854,10 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       $restrict_entity     = getEntitiesRestrictRequest(" AND", 'networkports');
       $query_fi_networkports = "SELECT COUNT(networkports.`id`) as nb_networkports
                              FROM glpi_networkports networkports
-                             INNER JOIN glpi_plugin_fusioninventory_networkports fi_networkports
+                             LEFT JOIN glpi_plugin_fusioninventory_networkports fi_networkports
                                ON fi_networkports.`networkports_id` = networkports.`id`
                              WHERE networkports.`is_deleted`  = '0' 
+                               fi_networkports.`id` IS NOT NULL
                                $restrict_entity";
       $res_fi_networkports = $DB->query($query_fi_networkports);
       if ($data_fi_networkports = $DB->fetch_assoc($res_fi_networkports)) {
@@ -890,9 +894,10 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       $restrict_entity     = getEntitiesRestrictRequest(" AND", 'networkports');
       $query_fi_networkports = "SELECT COUNT(networkports.`id`) as nb_networkports
                              FROM glpi_networkports networkports
-                             INNER JOIN glpi_plugin_fusioninventory_networkports fi_networkports
+                             LEFT JOIN glpi_plugin_fusioninventory_networkports fi_networkports
                                ON fi_networkports.`networkports_id` = networkports.`id`
                              WHERE networkports.`is_deleted`  = '0' 
+                               AND fi_networkports.`id` IS NOT NULL
                                AND (fi_networkports.`ifstatus`='1' 
                                     OR fi_networkports.`ifstatus`='up')
                                $restrict_entity";
