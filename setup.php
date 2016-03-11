bb28a6c0cec1c8b10dec65838b1f1851b8af5a54
amma35 <amandine.manceau@infotel.com>
2016-03-11 10:34:16 +0100

    Change the version

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_FUSIONINVENTORY_VERSION", "0.90+1.2");
+define ("PLUGIN_FUSIONINVENTORY_VERSION", "0.90+1.1");
 
 // Used for use config values in 'cache'
 $PF_CONFIG = array();
