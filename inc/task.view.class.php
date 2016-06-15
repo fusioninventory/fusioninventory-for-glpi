9904a77e97b279a8d0b98918a22df342d5c05d41
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-06-15 16:23:22 +0200

    separe timeslot in task into 2 fields (preparation & execution)
    wakeup agent is on exec timeslot
    
    typo
    
    typo

--- inc/task.view.class.php
+++ inc/task.view.class.php
@@ -494,9 +494,15 @@ class PluginFusioninventoryTaskView extends PluginFusioninventoryCommonView {
          );
 
          $this->showDropdownForItemtype(
-            __('Timeslot','fusioninventory'),
+            __('Preparation timeslot','fusioninventory'),
             "PluginFusioninventoryTimeslot",
-            array('value' => $this->fields['plugin_fusioninventory_timeslots_id'])
+            array('value' => $this->fields['plugin_fusioninventory_timeslots_prep_id'])
+            );
+
+         $this->showDropdownForItemtype(
+            __('Execution timeslot','fusioninventory'),
+            "PluginFusioninventoryTimeslot",
+            array('value' => $this->fields['plugin_fusioninventory_timeslots_exec_id'])
             );
 
          $this->showIntegerField( __('Agent wakeup interval (in minutes)', 'fusioninventory'), "wakeup_agent_time",
