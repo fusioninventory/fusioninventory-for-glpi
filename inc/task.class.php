d8967916bdb12b30cd9b2e3ea429d8215d52668f
David Durieux <d.durieux@siprossii.com>
2016-07-28 23:58:23 +0200

    Fix php error

--- inc/task.class.php
+++ inc/task.class.php
@@ -1386,6 +1386,7 @@ class PluginFusioninventoryTask extends PluginFusioninventoryTaskView {
     * @see CommonDBTM::post_updateItem()
    **/
    function post_updateItem($history=1) {
+       global $DB;
 
       if (isset($this->oldvalues['is_active'])
               && $this->oldvalues['is_active'] == 1) {
