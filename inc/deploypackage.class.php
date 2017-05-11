394e46abf13523aceda6fb61803b861c535e84ef
Walid <wnouh@teclib.com>
2017-05-11 18:58:47 +0200

    Display JSON package representation in a debug tab

--- inc/deploypackage.class.php
+++ inc/deploypackage.class.php
@@ -419,8 +419,7 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
     */
    function showMenu($options=array()) {
 
-      $this->displaylist = FALSE;
-
+      $this->displaylist  = FALSE;
       $this->fields['id'] = -1;
       $this->showList();
    }
@@ -451,11 +450,10 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
       if ($this->fields['plugin_fusioninventory_deploygroups_id'] > 0) {
          $this->addStandardTab(__CLASS__, $ong, $options);
       }
+
       $ong['no_all_tab'] = TRUE;
       return $ong;
    }
-
-
 
    /**
     * Display form
@@ -591,14 +589,6 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
          echo "</tr>";
       }
 
-      if ($_SESSION['glpi_use_mode'] == Session::DEBUG_MODE) {
-         // === debug ===
-         echo "<tr><td>";
-         echo "<span id='package_json_debug'>";
-         $this->displayJSONDebug();
-         echo "</sp3an>";
-         echo "</td></tr>";
-      }
       echo "</table>";
       if (!empty($error_msg)) {
          echo "</div>";
@@ -665,27 +655,27 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
     *
     * @global array $CFG_GLPI
     */
-   function displayJSONDebug() {
+   function showDebug() {
       global $CFG_GLPI;
 
-      if ($_SESSION['glpi_use_mode'] == Session::DEBUG_MODE) {
-
-         echo "<span class='red'><b>DEBUG</b></span>";
-         echo "<form action='".$CFG_GLPI["root_doc"].
-         "/plugins/fusioninventory/front/deploypackage.form.php' method='POST'>";
-         echo "<textarea cols='132' rows='25' style='border:0' name='json'>";
-         echo PluginFusioninventoryToolbox::formatJson($this->fields['json']);
-         echo "</textarea>";
-         if ($this->can($this->getID(), UPDATE)) {
-            echo "<input type='hidden' name='packages_id' value='{$this->fields['id']}' />";
-            echo "<input type='submit' name='update_json' value=\"".
-               _sx('button', 'Save')."\" class='submit'>";
-         }
-         Html::closeForm();
+      echo "<form action='".PluginFusioninventoryDeploypackage::getFormURL()
+         ."' method='POST'>";
+      echo "<table class='tab_cadre_fixe'>";
+      echo "<tr><th>".__('JSON package representation', 'fusioninventory')."</th></tr>";
+      echo "<tr><td>";
+      echo "<textarea cols='132' rows='50' style='border:1' name='json'>";
+      echo PluginFusioninventoryToolbox::formatJson($this->fields['json']);
+      echo "</textarea></td></tr>";
+      if ($this->can($this->getID(), UPDATE)) {
+         echo "<tr><td align='center'>";
+         echo Html::hidden('packages_id', ['value' => $this->fields['id']]);
+         echo "<input type='submit' name='update_json' value=\"".
+            _sx('button', 'Save')."\" class='submit'>";
+         echo "</td></tr>";
       }
+      echo "</table>";
+      Html::closeForm();
    }
-
-
 
    /**
     * Update the json structure
