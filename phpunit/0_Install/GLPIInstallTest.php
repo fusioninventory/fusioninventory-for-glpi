b3e8e86ae155a1bccf2bf290cad874a717e33bab
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-06-29 11:40:13 +0200

    Make plugin  dev compatibe with GLPI dev
    Fix install GLPI db

--- phpunit/0_Install/GLPIInstallTest.php
+++ phpunit/0_Install/GLPIInstallTest.php
@@ -1,5 +1,6 @@
 <?php
 require_once('commonfunction.php');
+include_once (GLPI_ROOT . "/inc/define.php");
 include_once (GLPI_ROOT . "/inc/based_config.php");
 include_once (GLPI_ROOT . "/inc/dbmysql.class.php");
 include_once (GLPI_CONFIG_DIR . "/config_db.php");
@@ -27,21 +28,22 @@ class GLPIInstallTest extends PHPUnit_Framework_TestCase {
       } else if ($glpi_version == '0.85/bugfixes') {
          $glpisql = '0.85.5';
       } else {
-         $glpisql = '9.2';
+         $glpisql = null;
       }
 
+      $sql_file = $glpisql === null ? "glpi-empty.sql" : "glpi-$glpisql-empty.sql";
       $result = load_mysql_file(
          $DBvars['dbuser'],
          $DBvars['dbhost'],
          $DBvars['dbdefault'],
          $DBvars['dbpassword'],
-         GLPI_ROOT ."/install/mysql/glpi-".$glpisql."-empty.sql"
+         GLPI_ROOT ."/install/mysql/$sql_file"
       );
 
       $output = array();
       $returncode = 0;
       exec(
-         "php -f ".GLPI_ROOT. "/tools/cliupdate.php -- --upgrade",
+         "php ".GLPI_ROOT. "/tools/cliupdate.php --dev --force",
          $output, $returncode
       );
       $this->assertEquals(0,$returncode,
