a59b6aef71915897253d3fd3a56fda1402fec1c6
Walid <wnouh@teclib.com>
2017-07-05 15:43:17 +0200

    Only restrict by entity if by_entites boolean is set to true

--- inc/task.class.php
+++ inc/task.class.php
@@ -1519,7 +1519,7 @@ class PluginFusioninventoryTask extends PluginFusioninventoryTaskView {
 
       // Filter by entity
       if (isset($filter['by_entities'])
-              && is_bool($filter['by_entities'])) {
+	      && (bool)$filter['by_entities']) {
          $where[] = getEntitiesRestrictRequest("", 'task');
       }
 
