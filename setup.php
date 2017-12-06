cb7dc3d9c39ffbe43e5ca67494a02e936c8e39b2
Walid <wnouh@teclib.com>
2017-12-06 12:16:21 +0100

    Switch to GLPI master branch (currently 9.3)

--- setup.php
+++ setup.php
@@ -45,7 +45,7 @@
  *
  */
 
-define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.2+1.0");
+define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.3+1.0");
 
 // Used for use config values in 'cache'
 $PF_CONFIG = [];
@@ -55,7 +55,7 @@ $PF_ESXINVENTORY = false;
 define ("PLUGIN_FUSIONINVENTORY_XML", '');
 
 define ("PLUGIN_FUSIONINVENTORY_OFFICIAL_RELEASE", "0");
-define ("PLUGIN_FUSIONINVENTORY_REALVERSION", "9.2+1.0 SNAPSHOT");
+define ("PLUGIN_FUSIONINVENTORY_REALVERSION", "9.3+1.0 SNAPSHOT");
 include_once(GLPI_ROOT."/inc/includes.php");
 
 define("PLUGIN_FUSIONINVENTORY_REPOSITORY_DIR",
@@ -487,8 +487,8 @@ function plugin_version_fusioninventory() {
            'homepage'       => 'https://github.com/fusioninventory/fusioninventory-for-glpi',
            'requirements'   => [
               'glpi' => [
-                 'min' => '9.2',
-                  'max' => '9.3',
+                 'min' => '9.3',
+                  'max' => '9.4',
                   'dev' => PLUGIN_FUSIONINVENTORY_OFFICIAL_RELEASE == 0
                ],
                'php' => [
@@ -516,7 +516,7 @@ function plugin_fusioninventory_check_prerequisites() {
 
    $version = rtrim(GLPI_VERSION, '-dev');
    if (version_compare($version, '9.2', 'lt')) {
-      echo "This plugin requires GLPI 9.2";
+      echo "This plugin requires GLPI 9.3";
       return false;
    }
 
