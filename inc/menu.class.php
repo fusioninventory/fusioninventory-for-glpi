54c73a2ec7e8bb667f07188210a59362259dec78
Frédéric MOHIER <frederic.mohier@gmail.com>
2018-01-18 17:04:26 +0100

    Fix #2308: make consistent messages "Equipment ignored on import" (#2406)

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -329,7 +329,7 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       }
 
       if (Session::haveRight('plugin_fusioninventory_ignoredimportdevice', READ)) {
-         $a_menu[2]['name'] = __('Ignored import devices', 'fusioninventory');
+         $a_menu[2]['name'] = __('Asset skipped during import', 'fusioninventory');
          $a_menu[2]['pic']  = $CFG_GLPI['root_doc']."/plugins/fusioninventory/pics/menu_rules.png";
          $a_menu[2]['link'] = Toolbox::getItemTypeSearchURL(
                     'PluginFusioninventoryIgnoredimportdevice'
