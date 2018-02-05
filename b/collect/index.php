cdfd3df2ca5612ddeedfefca498bf050510b896e
Fred Mohier <frederic.mohier@gmail.com>
2018-02-05 16:17:49 +0100

    Close #2471 - get collected information restricted entities recursively Close #2467 - collected information are no more 'nodisplay'

--- b/collect/index.php
+++ b/collect/index.php
@@ -173,14 +173,14 @@ switch (filter_input(INPUT_GET, "action")) {
                   $computers_id,
                   $a_values,
                   $sid
-               );               
+               );
             }
 
-            // change status of state table row
-            $pfTaskjobstate->changeStatus($jobstate['id'],
-                       PluginFusioninventoryTaskjobstate::AGENT_HAS_SENT_DATA);
+         // change status of state table row
+         $pfTaskjobstate->changeStatus($jobstate['id'],
+                    PluginFusioninventoryTaskjobstate::AGENT_HAS_SENT_DATA);
 
-            // add logs to job
+         // add logs to job
             if (count($a_values)) {
                $flag    = PluginFusioninventoryTaskjoblog::TASK_INFO;
                $message = json_encode($a_values, JSON_UNESCAPED_SLASHES);
@@ -188,13 +188,13 @@ switch (filter_input(INPUT_GET, "action")) {
                $flag    = PluginFusioninventoryTaskjoblog::TASK_ERROR;
                $message = __('Path not found', 'fusioninventory');
             }
-            $pfTaskjoblog->addTaskjoblog($jobstate['id'],
-                                         $jobstate['items_id'],
-                                         $jobstate['itemtype'],
-                                         $flag,
-                                         $message);
+         $pfTaskjoblog->addTaskjoblog($jobstate['id'],
+                                      $jobstate['items_id'],
+                                      $jobstate['itemtype'],
+                                      $flag,
+                                      $message);
          }
-         break;
+      break;
 
 
 
@@ -205,7 +205,7 @@ switch (filter_input(INPUT_GET, "action")) {
                                              $jobstate['items_id'],
                                              $jobstate['itemtype']);
 
-         break;
+      break;
       }
 
    if (count($response) > 0) {
