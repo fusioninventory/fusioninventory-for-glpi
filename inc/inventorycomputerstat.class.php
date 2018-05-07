56942b9627cb13da7f407b1679d527b0315a6ed6
David Durieux <d.durieux@siprossii.com>
2018-05-07 09:09:00 +0200

    Enhance home page statistics. closes #2444

--- inc/inventorycomputerstat.class.php
+++ inc/inventorycomputerstat.class.php
@@ -134,7 +134,7 @@ class PluginFusioninventoryInventoryComputerStat extends CommonDBTM {
          $result = $DB->query($query);
          $data = $DB->fetch_assoc($result);
          $a_counters['values'][] = array(
-             'label' => date('G', $timestampSearch)." ".__('hour'),
+             'label' => date('H', $timestampSearch).":00",
              'value' => (int)$data['counter']
          );
       }
