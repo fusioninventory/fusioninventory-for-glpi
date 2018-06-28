1fa1ec475e25517e1b1364d9a46e2cc2c796938f
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-06-28 11:01:10 +0200

    Bump year

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ var fifooter = "<br/> \
 <a class='copyright' href='http://fusioninventory.org/'> \
 FusionInventory 9.3+1.0 - Copyleft \
 <span style='display:inline-block;transform: rotate(180deg);font-size: 12px;'>&copy;</span> \
-2010-2017 by FusionInventory Team \
+2010-2018 by FusionInventory Team \
 </a>";
 
 $(window).bind("load", function() {
