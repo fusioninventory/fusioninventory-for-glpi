33c82968a35e6903e2bc931d04814bb5f3ed71dc
David Durieux <d.durieux@siprossii.com>
2018-11-28 11:42:56 +0100

    Add version in the footer

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,6 @@
 var fifooter = "<br/> \
 <a class='copyright' href='http://fusioninventory.org/'> \
-FusionInventory 9.3+1.1 - Copyleft \
+FusionInventory 9.3+1.3 - Copyleft \
 <span style='display:inline-block;transform: rotate(180deg);font-size: 12px;'>&copy;</span> \
 2010-2018 by FusionInventory Team \
 </a>";
