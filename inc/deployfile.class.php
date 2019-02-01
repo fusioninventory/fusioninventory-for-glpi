d392bbc8dcedf1fef8f378393e07da81fc27eb74
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-02-01 14:58:39 +0100

    Fix query

--- inc/deployfile.class.php
+++ inc/deployfile.class.php
@@ -1049,7 +1049,7 @@ class PluginFusioninventoryDeployFile extends PluginFusioninventoryDeployPackage
       $a_files = $this->find();
       foreach ($a_files as $data) {
          $cnt = countElementsInTable('glpi_plugin_fusioninventory_deploypackages',
-            ['json' => ['LIKE' => '%"' . $data['sha512'] . '"%']]);
+            ['json' => ['LIKE', '%"' . $data['sha512'] . '"%']]);
          if ($cnt == 0) {
             $this->delete($data);
             $manifest_filename = PLUGIN_FUSIONINVENTORY_MANIFESTS_DIR.$data['sha512'];
