0c91590e7906abd208ce4dade01e077278a7c8e4
David Durieux <david@durieux.family>
2019-11-03 21:49:51 +0100

    Prepare version 9.4+2.0

--- setup.php
+++ setup.php
@@ -45,7 +45,7 @@
  *
  */
 
-define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.4+1.1");
+define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.4+2.0");
 // Minimal GLPI version, inclusive
 define('PLUGIN_FUSIONINVENTORY_GLPI_MIN_VERSION', '9.4');
 // Maximum GLPI version, exclusive
