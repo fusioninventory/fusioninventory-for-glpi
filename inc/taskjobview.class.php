1f00f91880ff096f58afe9f0ec0baac566705adf
Cédric Anne <cedric.anne@gmail.com>
2019-11-14 10:32:22 +0100

    Whitelist autocomplete fields

--- inc/taskjobview.class.php
+++ inc/taskjobview.class.php
@@ -937,4 +937,25 @@ class PluginFusioninventoryTaskjobView extends PluginFusioninventoryCommonView {
    }
 
 
+   function rawSearchOptions() {
+
+      $tab = [];
+
+      $tab[] = [
+         'id'           => 'common',
+         'name'         => __('Characteristics')
+      ];
+
+      $tab[] = [
+         'id'           => '1',
+         'table'        => $this->getTable(),
+         'field'        => 'name',
+         'name'         => __('Name'),
+         'datatype'     => 'itemlink',
+         'autocomplete' => true,
+      ];
+
+      return $tab;
+   }
+
 }
