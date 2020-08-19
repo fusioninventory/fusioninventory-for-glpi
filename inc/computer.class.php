edaf18e2125d184a67184b8a6f4450724355004e
TuX1sPuNk <tux1spunk@protonmail.com>
2020-08-19 11:37:25 +0200

    Updated fix : used function Plugin::GetPhpDir() instead

--- inc/computer.class.php
+++ inc/computer.class.php
@@ -67,12 +67,8 @@ class PluginFusioninventoryComputer extends Computer {
       $plugin = new Plugin();
       if ($plugin->isInstalled('fields')) {
          if ($plugin->isActivated('fields')) {
-            // Check if Fields plugin is installed in marketplace or plugins folder
-            if (file_exists(GLPI_ROOT . "/marketplace/fields/hook.php")) {
-               include_once(GLPI_ROOT . "/marketplace/fields/hook.php");
-            } else {
-               include_once(GLPI_ROOT . "/plugins/fields/hook.php");
-            }
+            // Include Fields hook from correct installation folder (marketplace or plugins)
+            include_once(Plugin::GetPhpDir("fields") . "/hook.php");
             $options['fields_plugin'] = [
                'id'   => 'fields_plugin',
                'name' => __('Plugin fields')
