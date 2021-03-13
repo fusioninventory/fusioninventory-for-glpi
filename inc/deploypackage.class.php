a0e93c1061f8ddc65301cbe8959753426345c8ae
David Durieux <david@durieux.family>
2021-03-13 13:34:47 +0100

    Fix tab in packages because changes in GLPI. closes #3114

--- inc/deploypackage.class.php
+++ inc/deploypackage.class.php
@@ -461,13 +461,7 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
    function defineTabs($options = []) {
       $ong = [];
       $this->addDefaultFormTab($ong);
-      if ($this->fields['id'] > 0) {
-         $this->addStandardTab('PluginFusioninventoryDeployinstall', $ong, $options);
-      }
-      if ($this->fields['plugin_fusioninventory_deploygroups_id'] > 0) {
-         $this->addStandardTab(__CLASS__, $ong, $options);
-      }
-
+      $this->addStandardTab(__CLASS__, $ong, $options);
       $ong['no_all_tab'] = true;
       return $ong;
    }
@@ -1013,17 +1007,24 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
 
             case __CLASS__ :
                if ($item->canUpdateItem()) {
-                  if ($_SESSION['glpishow_count_on_tabs']) {
-                     $nb = $item->countVisibilities();
-                     return self::createTabEntry(_n('Target for deploy on demand',
-                                                    'Targets for deploy on demand',
-                                                    $nb, 'fusioninventory'),
-                                                    $nb);
-                  } else {
-                     return _n('Target for deploy on demand',
-                               'Targets for deploy on demand', 2,
-                               'fusioninventory');
+                  $tabs = [];
+                  if ($item->fields['id'] > 0) {
+                     $tabs[1] = __('Package actions', 'fusioninventory');
                   }
+                  if ($item->fields['plugin_fusioninventory_deploygroups_id'] > 0) {
+                     if ($_SESSION['glpishow_count_on_tabs']) {
+                        $nb = $item->countVisibilities();
+                        $tabs[2] = self::createTabEntry(_n('Target for deploy on demand',
+                                                      'Targets for deploy on demand',
+                                                      $nb, 'fusioninventory'),
+                                                      $nb);
+                     } else {
+                        $tabs[2] = _n('Target for deploy on demand',
+                                 'Targets for deploy on demand', 2,
+                                 'fusioninventory');
+                     }
+                  }
+                  return $tabs;
                }
 
             case 'Computer':
@@ -1049,8 +1050,11 @@ class PluginFusioninventoryDeployPackage extends CommonDBTM {
    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0) {
 
       if ($item->getType() == __CLASS__) {
-         if ($tabnum == 1) {
+         if ($tabnum == 2) {
             $item->showVisibility();
+            return true;
+         } else if ($tabnum == 1) {
+            $item->displayOrderTypeForm();
             return true;
          }
       } else if ($item->getType() == 'Computer') {
