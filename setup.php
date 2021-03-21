65bed59030a523007c31fd9c6c38386c7ab6357e
David Durieux <david@durieux.family>
2021-03-21 20:34:23 +0100

    Prepare version 9.4+2.6

--- setup.php
+++ setup.php
@@ -45,7 +45,7 @@
  *
  */
 
-define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.4+2.5");
+define ("PLUGIN_FUSIONINVENTORY_VERSION", "9.4+2.6");
 // Minimal GLPI version, inclusive
 define('PLUGIN_FUSIONINVENTORY_GLPI_MIN_VERSION', '9.4');
 // Maximum GLPI version, exclusive
