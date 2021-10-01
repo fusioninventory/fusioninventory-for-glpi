a737bc38eed3e2154f5817f437695b65983e7437
Johan Cwiklinski <jcwiklinski@teclib.com>
2021-10-01 09:11:22 +0200

    Fix unescaped data in SQL query

--- inc/inventorycomputerlib.class.php
+++ inc/inventorycomputerlib.class.php
@@ -1629,7 +1629,7 @@ class PluginFusioninventoryInventoryComputerLib extends PluginFusioninventoryInv
                $arrays['entities_id'] = $entities_id;
                $arrays['otherserial'] = PluginFusioninventoryToolbox::setInventoryNumber(
                   'Peripheral', '', $entities_id);
-               $a_peripherals[] = $peripheral->add($arrays);
+               $a_peripherals[] = $peripheral->add(\Toolbox::addslashes_deep($arrays));
             } else {
                $a_peripherals[] = $data['found_equipment'][0];
             }
