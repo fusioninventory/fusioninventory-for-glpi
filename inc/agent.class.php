56ba604a61e56e80bf090514360c54067fff7681
nelero <aurelien.pohu@gmail.com>
2021-12-10 12:33:35 +0100

    Fix Lint 8.0 errors with spaces at end of line
    lines 1230 and 1232 : extra spaces before end of line removed.

--- inc/agent.class.php
+++ inc/agent.class.php
@@ -1227,9 +1227,9 @@ class PluginFusioninventoryAgent extends CommonDBTM {
             'last_contact' => ['<', new QueryExpression("date_add(now(), interval -".$retentiontime." day)")]
          ]
       ]);
-      
+
       $cron_status = false;
-      if (count($iterator)) {   
+      if (count($iterator)) {
          $action = $pfConfig->getValue('agents_action');
          if ($action == PluginFusioninventoryConfig::ACTION_CLEAN) {
             //delete agents
@@ -1257,4 +1257,3 @@ class PluginFusioninventoryAgent extends CommonDBTM {
 
 
 }
-
