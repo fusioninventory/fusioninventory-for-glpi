242ed4976ff52efaccfd2412a6700f6ccb9a4791
David Durieux <david@durieux.family>
2022-05-10 08:14:10 +0200

    Fix documentation links

--- inc/menu.class.php
+++ inc/menu.class.php
@@ -229,15 +229,15 @@ class PluginFusioninventoryMenu extends CommonGLPI {
       if ($cronTask->fields['lastrun'] == ''
               OR strtotime($cronTask->fields['lastrun']) < strtotime("-3 day")) {
          $message = __('GLPI cron not running, see ', 'fusioninventory');
-         $message .= " <a href='http://fusioninventory.org/documentation/fi4g/cron.html' target='_blank'>".__('documentation', 'fusioninventory')."</a>";
+         $message .= " <a href='https://documentation.fusioninventory.org/%20FusionInventory_for_GLPI/%20%20Installation%20%26%20update/3.cron/' target='_blank'>".__('documentation', 'fusioninventory')."</a>";
          Html::displayTitle($CFG_GLPI['root_doc']."/pics/warning.png", $message, $message);
       }
 
       // Check if plugin right updated (because security problems)
       $fi_php_path = Plugin::getPhpDir('fusioninventory');
       if (file_exists($fi_php_path."/ajax/deploydropdown_operatingsystems.php")) {
-         $message = __('SECURITY PROBLEM, see `2.1 Update` section to update correctly the plugin on ', 'fusioninventory');
-         $message .= " <a href='http://fusioninventory.org/documentation/fi4g/installation.html' target='_blank'>".__('documentation', 'fusioninventory')."</a>";
+         $message = __('SECURITY PROBLEM, see `Update` page to update correctly the plugin on ', 'fusioninventory');
+         $message .= " <a href='https://documentation.fusioninventory.org/%20FusionInventory_for_GLPI/%20%20Installation%20%26%20update/2.update/' target='_blank'>".__('documentation', 'fusioninventory')."</a>";
          Html::displayTitle($CFG_GLPI['root_doc']."/pics/warning.png", $message, $message);
       }
 
