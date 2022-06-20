272a7e74c4b40b3b1bfb22c8bfbf4f7b81a4cd54
David Durieux <david@durieux.family>
2022-06-20 07:23:03 +0200

    Set default number of lines for command in package deploy from 10 to all. Fix display value too.

--- inc/deployaction.class.php
+++ inc/deployaction.class.php
@@ -223,6 +223,14 @@ class PluginFusioninventoryDeployAction extends PluginFusioninventoryDeployPacka
                if ($key ==="exec") {
                   echo "<pre style='border-left:solid lightgrey 3px;margin-left: 5px;".
                           "padding-left:2px;white-space: pre-wrap;'>$value</pre>";
+               } else if ($key == 'logLineLimit') {
+                  if ($value == 0) {
+                     echo ' '.__('None');
+                  } else if ($value == -1) {
+                     echo ' '.__('All');
+                  } else {
+                     echo " $value ";
+                  }
                } else {
                   echo " $value ";
                }
@@ -440,7 +448,7 @@ class PluginFusioninventoryDeployAction extends PluginFusioninventoryDeployPacka
                      'max'   => 5000,
                      'step'  => 10,
                      'toadd' => [0 => __('None'), -1 => __('All')],
-                     'value' => (isset($config_data['logLineLimit']))?$config_data['logLineLimit']:10
+                     'value' => (isset($config_data['logLineLimit']))?$config_data['logLineLimit']:-1
                     ];
          Dropdown::showNumber('logLineLimit', $options);
          echo "&nbsp;<span class='red'><i>";
